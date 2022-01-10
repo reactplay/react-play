09d17336cda89db18646cde0005ffdaf889c8922
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-01-10 12:51:33 +0530

    ✨ Added the movies.json

---
+++ src/data/movies.js
@@ -0,0 +1,66 @@
+
+
+export const movies = [
+  {
+    id: 1,
+    title: 'The Matrix',
+    year: 1999,
+    director: 'Lana Wachowski',
+    rating: 'R',
+    runtime: 136,
+    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg'
+  },
+  {
+    id: 2,
+    title: 'The Lord of the Rings',
+    year: 2001,
+    director: 'Peter Jackson',
+    rating: 'PG-13',
+    runtime: 178,
+    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg'
+  },
+  {
+    id: 3,
+    title: 'The Dark Knight',
+    year: 2008,
+    director: 'Christopher Nolan',
+    rating: 'PG-13',
+    runtime: 152,
+    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg'
+  },
+  {
+    id: 4,
+    title: 'Inception',
+    year: 2010,
+    director: 'Christopher Nolan',
+    rating: 'PG-13',
+    runtime: 148,
+    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg'
+  },
+  {
+    id: 5,
+    title: 'Interstellar',
+    year: 2014,
+    director: 'Christopher Nolan',
+    rating: 'PG-13',
+    runtime: 169,
+    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg'
+  },
+  {
+    id: 6,
+    title: 'The Dark Knight Rises',
+    year: 2012,
+    director: 'Christopher Nolan',
+    rating: 'PG-13',
+    runtime: 164,
+    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_SX300.jpg'
+  }, 
+  {
+    id: 7,
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    year: 2001,
+    director: 'Peter Jackson',
+    rating: 'PG-13',
+    runtime: 178,
+    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg'
+  }];
