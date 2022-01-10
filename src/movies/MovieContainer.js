615fbff71bbf034c4efd8baa2f66c2461dd4a52e
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-01-10 15:26:02 +0530

    ✨ The movie container loads data and show to UI

--- src/movies/MovieContainer.js
+++ src/movies/MovieContainer.js
@@ -1,6 +1,9 @@
 
 
+import { useEffect, useState } from 'react';
 import { movies } from '../data/movies';
+import './movies.css';
+
 
 const fetchMovies = () => {
   return movies;
@@ -8,10 +11,27 @@ const fetchMovies = () => {
 
 const MovieContainer = () => {
   console.log(fetchMovies());
-  
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const movies = fetchMovies();
+    console.log('MovieContainer: useEffect: movies: ', movies);
+    setMovies(movies);
+  }, []);
+
   return(
     <div className="movie-container">
-      <h2>Movie Container</h2>
+      <h2>Movies</h2>
+      <ul className ="movie-list">
+        {movies.map(movie => (
+          <li key={movie.id} className="movie">
+            <img src={movie.poster} alt={movie.title} />
+            <p>{movie.title} by {movie.description} was released on {movie.year}</p>
+            <p>Rating: {movie.rating}</p>
+          </li>
+        ))}
+      </ul>      
     </div>
   );
 };
