e4480cf144b0910de8f95d6334c1cf71a3c3ba23
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-01-31 12:29:16 +0530

    ✨ Added a datetime picker for countdown timer

---
+++ src/plays/date-time-counter/CdTimerComp.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import CountDownTimer from "./CountDownTimer";
+
+const CdTimerComp = () => {
+  const dateTimeAfterThreeDays = new Date().getTime() + 259200000;
+  const [targetDate, setTargetDate] = useState(
+    new Date(dateTimeAfterThreeDays)
+  );
+
+ 
+  const handleChange = (event) => {
+    event.preventDefault();
+    setTargetDate(new Date(event.target.value));
+  };
+
+  return (
+    <div className="countdown-container">
+      <h2>Countdown Timer</h2>
+      <form>
+        <label for="countdown-date-time">Select a Date and Time:</label>
+        <input
+          type="datetime-local"
+          id="countdown-date-time"
+          name="countdown-date-time"
+          onChange={handleChange}
+        />
+      </form>
+      <CountDownTimer targetDate={targetDate} />
+    </div>
+  );
+};
+
+export default CdTimerComp;
