0f364810d4c99ae5972ab7cd879cb46b37cf7def
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-02-06 09:18:02 +0530

    Update Header.js

--- src/common/Header.js
+++ src/common/Header.js
@@ -6,14 +6,14 @@ const Header = () => {
   return (
     <header className="app-header">
         <span><Link to="/">React Play</Link></span>
-        <ul className="header-links">
+    {/*<ul className="header-links">
           <li>
             <a href="https://github.com/atapas/react-play" target="_blank" rel="noopener noreferrer">GitHub</a>
           </li>
           <li>
             <a href="https://twitter.com/tapasadhikary" target="_blank" rel="noopener noreferrer">Twitter</a>
           </li>
-        </ul>
+        </ul>*/}
     </header>
   );
 };
