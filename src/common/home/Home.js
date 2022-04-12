1b50e3c429dc768d9b4eedfe1f7d198ee69f6a28
Nirmal Kumar <nirmalkc@gmail.com>
2022-04-12 08:44:01 +0530

    home page edits

--- src/common/home/Home.js
+++ src/common/home/Home.js
@@ -18,58 +18,62 @@ const Home = () => {
           <Flower className="home-bg-graphics-rg" />
           <Flower className="home-bg-graphics-lg" />  
         </div>
-        <h2 className="body-title">Start <strong>React Code</strong> Arena <br/>with ReactPlay</h2>
-        <p className="body-desc">
-          React Play is an opensource application to learn, contribute and give back to 
-          the React developer community. Do you want to know more on how it works? Please check out
-          the video below.</p>
-        <div className='body-c2a'>
-          <Link to="/plays" className="body-c2a-btn body-c2a-btn--primary">
-            <MdManageSearch className="icon" />
-            <span className="btn-label">Browse</span>
-          </Link>
-          <a
-            href="https://www.youtube.com/watch?v=rysTbzKOEO0"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="body-c2a-btn"
-          >
-            <IoPlayOutline className="icon" />
-            <span className="btn-label">See how it works</span>
-          </a>
-        </div>        
-        <ul className="home-features">
-          <li className="home-features-item">
-            <div className="item-icon">
-              <RiSlideshow4Line className="icon" color='var(--color-neutral-10)' />
-            </div>
-            <h3 className="item-title">Learn </h3>
-            <p className="item-desc">
-              Learn how to "Think in React" and build applications inspired by several plays(examples and demos).
-              Get to the source code of it, find related article, or even a YouTube video.
-            </p>
-          </li>
-          <li className="home-features-item">
-            <div className="item-icon">
-              <BiAddToQueue className="icon" color='var(--color-neutral-10)' />
-            </div>
-            <h3 className="item-title">Create </h3>
-            <p className="item-desc">
-              Create your own play and own it by following a few simple steps. Learned something new? 
-              Perfect to present as a play. You can also contribute to the existing plays.
-            </p>
-          </li>
-          <li className="home-features-item">
-            <div className="item-icon">
-              <BiShareAlt className="icon" color='var(--color-neutral-10)' />
-            </div>
-            <h3 className="item-title">Socialize </h3>
-            <p className="item-desc">
-              Share your plays with the community. You can share your plays on social media platforms 
-              like Facebook, Twitter, LinkedIn, to name a few, just with a single click.
-            </p>
-          </li>
-        </ul>
+        <div className='app-home-body-content'>
+          <h2 className="body-title">Start <strong>React Code</strong> Arena <br/>with ReactPlay</h2>
+          <p className="body-desc">
+            React Play is an opensource application to learn, contribute and give back to 
+            the React developer community. Do you want to know more on how it works? Please check out
+            the video below.</p>
+          <div className='body-c2a'>
+            <Link to="/plays" className="body-c2a-btn body-c2a-btn--primary">
+              <MdManageSearch className="icon" />
+              <span className="btn-label">Browse</span>
+            </Link>
+            <a
+              href="https://www.youtube.com/watch?v=rysTbzKOEO0"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="body-c2a-btn"
+            >
+              <IoPlayOutline className="icon" />
+              <span className="btn-label">See how it works</span>
+            </a>
+          </div>        
+        </div>
+      </section>
+      <section className='home-features'>
+        <ul className="list-home-features">
+            <li className="home-features-item">
+              <div className="item-icon">
+                <RiSlideshow4Line className="icon" color='var(--color-neutral-90)' />
+              </div>
+              <h3 className="item-title">Learn </h3>
+              <p className="item-desc">
+                Learn how to "Think in React" and build applications inspired by several plays(examples and demos).
+                Get to the source code of it, find related article, or even a YouTube video.
+              </p>
+            </li>
+            <li className="home-features-item">
+              <div className="item-icon">
+                <BiAddToQueue className="icon" color='var(--color-neutral-90)' />
+              </div>
+              <h3 className="item-title">Create </h3>
+              <p className="item-desc">
+                Create your own play and own it by following a few simple steps. Learned something new? 
+                Perfect to present as a play. You can also contribute to the existing plays.
+              </p>
+            </li>
+            <li className="home-features-item">
+              <div className="item-icon">
+                <BiShareAlt className="icon" color='var(--color-neutral-90)' />
+              </div>
+              <h3 className="item-title">Socialize </h3>
+              <p className="item-desc">
+                Share your plays with the community. You can share your plays on social media platforms 
+                like Facebook, Twitter, LinkedIn, to name a few, just with a single click.
+              </p>
+            </li>
+          </ul>
       </section>
       <section className="home-plays">
         <h3 className="plays-title-primary">Trending <strong>Plays</strong></h3>
@@ -90,6 +94,56 @@ const Home = () => {
         <div className="home-plays-footer">
           <Link to="/plays" className="home-anchor"><span className="text">View all ReactPlays</span></Link>
         </div>
+      </section>
+      <section className='home-contributors'>
+        <h3 className="title-primary"><strong><span>Big Thanks</span></strong><br/> to All Contributors!</h3>
+        <ul className='list-contributors'>
+          <li className='contributor'>
+            <a href='#' className='contributor-anchor'>
+              <img src='https://plopjs.com/images/avatars/amwmedia.jpg' alt='Name' className='contributor-thumb' />
+            </a>
+          </li>
+          <li className='contributor'>
+            <a href='#' className='contributor-anchor'>
+              <img src='https://plopjs.com/images/avatars/amwmedia.jpg' alt='Name' className='contributor-thumb' />
+            </a>
+          </li>
+          <li className='contributor'>
+            <a href='#' className='contributor-anchor'>
+              <img src='https://plopjs.com/images/avatars/amwmedia.jpg' alt='Name' className='contributor-thumb' />
+            </a>
+          </li>
+          <li className='contributor'>
+            <a href='#' className='contributor-anchor'>
+              <img src='https://plopjs.com/images/avatars/amwmedia.jpg' alt='Name' className='contributor-thumb' />
+            </a>
+          </li>
+          <li className='contributor'>
+            <a href='#' className='contributor-anchor'>
+              <img src='https://plopjs.com/images/avatars/amwmedia.jpg' alt='Name' className='contributor-thumb' />
+            </a>
+          </li>
+          <li className='contributor'>
+            <a href='#' className='contributor-anchor'>
+              <img src='https://plopjs.com/images/avatars/amwmedia.jpg' alt='Name' className='contributor-thumb' />
+            </a>
+          </li>
+          <li className='contributor'>
+            <a href='#' className='contributor-anchor'>
+              <img src='https://plopjs.com/images/avatars/amwmedia.jpg' alt='Name' className='contributor-thumb' />
+            </a>
+          </li>
+          <li className='contributor'>
+            <a href='#' className='contributor-anchor'>
+              <img src='https://plopjs.com/images/avatars/amwmedia.jpg' alt='Name' className='contributor-thumb' />
+            </a>
+          </li>
+          <li className='contributor'>
+            <a href='#' className='contributor-anchor'>
+              <img src='https://plopjs.com/images/avatars/amwmedia.jpg' alt='Name' className='contributor-thumb' />
+            </a>
+          </li>
+        </ul>
       </section>
     </div>
   );
