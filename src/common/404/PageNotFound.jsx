b37d05bebb6e08bb44babdf2ec1f42889dea4e59
Nirmal Kumar <nirmalkc@gmail.com>
2022-04-17 14:18:53 +0530

    adding 404 styling

--- src/common/404/PageNotFound.jsx
+++ src/common/404/PageNotFound.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
+import { ReactComponent as Image404 } from "images/img-404.svg";
+import "./404.css";
 
 const PageNotFound = () => {
 
   return(
-    <main style={{ padding: "1rem" }}>
-      <p>OOPs!!! Look like you are lost.</p>
-      <p>
-        Why don't you go back to <Link to="/">home</Link>?
+    <main className='page-404'>
+      <Image404 className="page-404-image" />
+      <p className='page-404-lead'>Look like you are lost</p>
+      <p className='page-404-desc'>
+        Why don't you go back to <Link to="/" className='page-404-link'>home</Link>?
       </p>
     </main>
   );
