f45619dc1ff03dc1e5b5ff4bbb21a6dbc7c1bbb6
Nirmal Kumar <nirmalkc@gmail.com>
2022-04-19 09:36:57 +0530

    adding homepage video

--- src/common/home/Home.jsx
+++ src/common/home/Home.jsx
@@ -63,6 +63,11 @@ const Home = () => {
             </a>
             */}
           </div>
+          <div className="video">
+            <div className="video-wrapper">
+              <iframe width="560" height="315" className="video-iframe" src="https://www.youtube.com/embed/XrWonrp296w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
         </div>
       </section>
       <section className="home-features">
