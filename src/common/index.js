0f666c534c289cb15bfcf1609709c2066eb7fcab
Lily <lolpo12345@outlook.com>
2022-04-20 22:20:18 +0300

    Add DefMeta to index.js

--- src/common/index.js
+++ src/common/index.js
@@ -3,6 +3,7 @@ import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import Home from "./home/Home";
 import Modal from "./modal";
+import DefMeta from "meta/DefMeta";
 import PlayMeta from "./playlists/PlayMeta";
 
-export { Header, Footer, Home, PageNotFound, Modal, PlayMeta };
+export { Header, Footer, Home, PageNotFound, Modal, PlayMeta, DefMeta };
