8c231339fde365169b7bf8f4e908ff214553d0fe
Lily <lolpo12345@outlook.com>
2022-04-20 22:51:14 +0300

    Fix faulty pathfile for PlayMeta

--- src/common/playlists/PlayMeta.jsx
+++ src/common/playlists/PlayMeta.jsx
@@ -1,10 +1,16 @@
 import { cloneElement, useEffect } from "react";
 import { Helmet } from "react-helmet";
 
-function PlayMeta({ id, name, description, cover, component }) {
+function PlayMeta({ id, name, description, path, cover, component }) {
+  const playFolder = path.split("/")[2];
+  const metaImage =
+    cover ||
+    `https://www.reactplay.io/${require(`../../plays/${playFolder}/cover.png`)}`;
+
   useEffect(() => {
     console.log("Play Meta in action!");
-  }, [name]);
+    console.log(metaImage);
+  }, [name, metaImage]);
 
   // cloneElement(play.component(), { id: play.id });
 
@@ -14,10 +20,25 @@ function PlayMeta({ id, name, description, cover, component }) {
         <meta name="description" content={description} />
         <meta property="og:title" content={name} />
         <meta property="og:description" content={description} />
+        <meta
+          property="og:image"
+          content={metaImage}
+          data-react-helmet="true"
+        />
+        <meta
+          property="og:image:alt"
+          content={description}
+          data-react-helmet="true"
+        />
         <meta name="twitter:title" content={name} data-react-helmet="true" />
         <meta
           name="twitter:description"
           content={description}
+          data-react-helmet="true"
+        />
+        <meta
+          name="twitter:image"
+          content={metaImage}
           data-react-helmet="true"
         />
       </Helmet>
