f14cbc8fb847467ab8b4d22b97361aef30b30231
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-04-21 18:04:20 +0530

    ✅ Update to React 18

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import RouteDefs from "common/routing/RouteDefs";
 import { SearchContext } from "common/search/search-context";
 import "index.css";
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import reportWebVitals from "reportWebVitals";
 
 const Index = () => {
@@ -22,8 +22,8 @@ const Index = () => {
     </React.StrictMode>
   );
 };
-
-ReactDOM.render(<Index />, document.getElementById("root"));
+const container = document.getElementById("root");
+createRoot(container).render(<Index />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
