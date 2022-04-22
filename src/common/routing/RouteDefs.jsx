521e50ec58a6f2c7832d2dff52f34d565c19b903
Hasnain Makada <82728823+hasnainmakada-99@users.noreply.github.com>
2022-04-22 11:12:15 +0530

    Resolved conflicts

--- src/common/routing/RouteDefs.jsx
+++ src/common/routing/RouteDefs.jsx
@@ -1,13 +1,7 @@
 import App from "App";
-<<<<<<< HEAD
 import { Footer, Header, Home, PageNotFound, PlayNotFound} from "common";
 import PlayList from 'common/playlists/PlayList';
 import { getAllPlays } from 'meta/play-meta-util';
-=======
-import { Footer, Header, Home, PageNotFound } from "common";
-import PlayList from "common/playlists/PlayList";
-import { getAllPlays } from "meta/play-meta-util";
->>>>>>> 21363533eeec11e0a8d0814b7490bc91e76f1fef
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { cloneElement } from 'react'
 
