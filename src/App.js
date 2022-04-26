6c1be83fb110ae54386607e650f41d581354e526
Abhishek Khatri <abhikhatri67@gmail.com>
2022-04-26 22:52:04 +0530

    Added google analytics tracking with yarn ga.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "App.css";
 import { Outlet } from "react-router-dom";
+import ReactGA from 'react-ga';
+
+import "App.css";
+
+const TRACKING_ID = "UA-XXXXX-X"; // TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  if (!window.location.href.includes("localhost")) {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
   return (
     <div className="app-body">
       <Outlet />
