6f6213163882de31db9fa56fb8659fdcf3886138
ShivamKatare <katare27451@gmail.com>
2022-04-29 11:23:03 +0530

    Created hooks folder for custom hooks

--- src/plays/keeper/Hooks/useLocalStorage.js
+++ src/plays/keeper/Hooks/useLocalStorage.js
@@ -6,7 +6,7 @@ function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       // Get from local storage by key
-      const item = window.localStorage.getItem(key);
+      const item = localStorage.getItem(key);
       // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
