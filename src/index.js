f95ee5a298150d5d9e9a3b16c133eef1f66a7f65
Shivam Katare <91705825+Shivam-Katare@users.noreply.github.com>
2022-04-29 20:16:48 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import RouteDefs from "common/routing/RouteDefs";
 import { SearchContext } from "common/search/search-context";
 import "index.css";
 import React, { useState } from "react";
-
 import { createRoot } from 'react-dom/client';
-
 import reportWebVitals from "reportWebVitals";
 
 const Index = () => {
@@ -15,7 +13,7 @@ const Index = () => {
     creator: "",
   });
 
-  const value = { searchTerm, setSearchTerm, filterQuery };
+  const value = { searchTerm, setSearchTerm, filterQuery, setFilterQuery };
   return (
     <React.StrictMode>
       <SearchContext.Provider value={value}>
@@ -24,7 +22,6 @@ const Index = () => {
     </React.StrictMode>
   );
 };
-
 const container = document.getElementById("root");
 createRoot(container).render(<Index />);
 
