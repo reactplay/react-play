9b1192800dc5f01c003183b20d850e48cfe5c7e0
Deepak Pundir <sendit2deepak@gmail.com>
2022-04-30 18:01:00 +0530

    requested changes //removed console.log/added dependecy array to useEffect hook

--- src/plays/analog-clock/AnalogClock.jsx
+++ src/plays/analog-clock/AnalogClock.jsx
@@ -19,11 +19,10 @@ function AnalogClock(props) {
     }, 1000);
 
     return () => clearInterval(interval);
-  });
+  }, []);
   const hour = date.getHours() * 30 + 180; // 360/12 = 30 each hour it rotates 30deg +180 (initial design at 0 deg was upside down so to fix it i rotate 180deg..// see the design at 0deg for more clarification)
   const minute = date.getMinutes() * 6 + 180; // 360/60 = 6 each minutes it rotate 6deg + 180(same as above to ...see at 0 deg)
   const second = date.getSeconds() * 6 + 180; //// 360/60 = 6 each minutes it rotate 6deg + 180(look at 0 deg for more clarification)
-  console.log(date.getHours());
 
   return (
     <>
