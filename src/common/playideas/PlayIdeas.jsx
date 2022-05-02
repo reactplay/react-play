fb6bcd687515233246d523efeba99b0c04fcfb06
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-05-02 18:26:06 +0530

    ✨ Started to build the idea page

--- src/common/playideas/PlayIdeas.jsx
+++ src/common/playideas/PlayIdeas.jsx
@@ -1,10 +1,51 @@
+import { useState, useEffect } from 'react';
+
+import data from './ideas.json';
+
+
 const PlayIdeas = () => {
+  const [ideas, setIdeas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    try {
+      async function fetchData() {
+        //const response = await fetch('./ideas.json');
+        //const json = await response.json();
+        const json = data.ideas;
+        setIdeas(json);
+        setIsLoading(false);
+      }
+      fetchData();
+    } catch (error) {
+      console.log(error);
+      setIsError(true);
+      setIsLoading(false);
+    }
+  }, []);
+
+  if (isError) {
+    return <div>Something went wrong ...</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+
   return (
     <div>
       <h1>Play Ideas</h1>
-      <p>
-        This is the Play Ideas page.
-      </p>
+      <ul>
+        {ideas.map((idea) => (
+          <li key={idea.id}>
+            <h2>{idea.title}</h2>
+            <p>{idea.description}</p>
+            <p>{idea.level}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
