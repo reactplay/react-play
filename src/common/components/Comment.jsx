a254877561dd132769b83f7c9b333e4e800c4c48
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-05-04 08:52:54 +0530

    ✨ Now the comment show-hide fixed

--- src/common/components/Comment.jsx
+++ src/common/components/Comment.jsx
@@ -1,36 +1,27 @@
 import Giscus from "@giscus/react";
-import { MdClose } from "react-icons/md";
 
-const Comment= () => {
+const Comment = () => {
   const projectRepoId = process.env.REACT_APP_GISCUS_PROJECT_REPO_ID;
-  const discussionCategoryId = process.env.REACT_APP_GISCUS_DISCUSSION_CATEGORY_ID;
-  const discussionCategoryName = process.env.REACT_APP_GISCUS_DISCUSSION_CATEGORY_NAME;
+  const discussionCategoryId =
+    process.env.REACT_APP_GISCUS_DISCUSSION_CATEGORY_ID;
+  const discussionCategoryName =
+    process.env.REACT_APP_GISCUS_DISCUSSION_CATEGORY_NAME;
 
   return (
     <>
-      <div className="play-details-comments">
-        <div className="comments-header">
-          <h3 className="header-title">Comments &amp; Reactions</h3>
-          <button className="header-action">
-            <MdClose size={24} className="icon" />
-          </button>
-        </div>
-        <div className="comments-body">
-          <Giscus
-            repo="atapas/react-play"
-            repoId={ projectRepoId }
-            category={ discussionCategoryName }
-            categoryId={ discussionCategoryId }
-            mapping="pathname"
-            reactionsEnabled="0"
-            emitMetadata="1"
-            inputPosition="top"
-            theme="light"
-            lang="en"
-            loading="lazy"
-          />
-        </div>
-      </div>
+      <Giscus
+        repo="atapas/react-play"
+        repoId={projectRepoId}
+        category={discussionCategoryName}
+        categoryId={discussionCategoryId}
+        mapping="pathname"
+        reactionsEnabled="0"
+        emitMetadata="1"
+        inputPosition="top"
+        theme="light"
+        lang="en"
+        loading="lazy"
+      />
     </>
   );
 };
--- src/common/playlists/PlayHeaderActions.jsx
+++ src/common/playlists/PlayHeaderActions.jsx
@@ -1,58 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { BsGithub } from "react-icons/bs";
 import { IoLogoYoutube } from "react-icons/io";
 import { AiOutlineRead } from "react-icons/ai";
 import { BiComment } from "react-icons/bi";
+import { MdClose } from "react-icons/md";
 
-import Comment from 'common/components/Comment';
+import Comment from "common/components/Comment";
 
-const PlayHeaderActions = ({play}) => {
+const PlayHeaderActions = ({ play }) => {
   console.log(play);
   const [showComment, setShowComment] = useState(false);
 
-  return(
+  return (
     <>
-      { 
-        <button
-          className='action badged'
-          onClick={() => setShowComment(true)}>
-            <BiComment className="icon" size="24px" />
-            <div className="badge-count">99</div>
-            <span className="sr-only">Comments</span>
-         </button>
+      {
+        <button className="action badged" onClick={() => setShowComment(true)}>
+          <BiComment className="icon" size="24px" />
+          {/*<div className="badge-count">99</div>*/}
+          <span className="sr-only">Comments</span>
+        </button>
       }
-      { play.path && 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className='action'
-            href={`https://github.com/atapas/react-play/tree/main/src${play.path}`}>
-              <BsGithub className="icon" size="24px" />
-              <span className="sr-only">GitHub</span>
-          </a>
-      }
-      { play.blog && 
-          <a
+      {play.path && (
+        <a
           target="_blank"
-          rel="noopener noreferrer" 
-          className='action'
-          href={play.blog}>
+          rel="noopener noreferrer"
+          className="action"
+          href={`https://github.com/atapas/react-play/tree/main/src${play.path}`}
+        >
+          <BsGithub className="icon" size="24px" />
+          <span className="sr-only">GitHub</span>
+        </a>
+      )}
+      {play.blog && (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="action"
+          href={play.blog}
+        >
           <AiOutlineRead className="icon" size="24px" />
           <span className="sr-only">Blog</span>
-          </a>
-      }
-      { play.video && 
-          <a
-            target="_blank"
-            rel="noopener noreferrer" 
-            className='action'
-            href={play.video}>
-              <IoLogoYoutube className="icon" size="24px" />
-              <span className="sr-only">Video</span>
-          </a>
-      }
-      { showComment && <Comment /> }
-    </>  
+        </a>
+      )}
+      {play.video && (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="action"
+          href={play.video}
+        >
+          <IoLogoYoutube className="icon" size="24px" />
+          <span className="sr-only">Video</span>
+        </a>
+      )}
+      {showComment && (
+        <div className="play-details-comments">
+          <div className="comments-header">
+            <h3 className="header-title">Comments</h3>
+            <button
+              className="header-action"
+              onClick={() => setShowComment(false)}
+            >
+              <MdClose size={24} className="icon" />
+            </button>
+          </div>
+          <div className="comments-body">
+            <Comment />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
