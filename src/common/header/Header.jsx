bdf06e07a6f4793b2834ba377be6c59c5314d47c
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-05-06 10:24:02 +0530

    ✨ Fixed the header issue

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,47 +1,69 @@
-import FilterPlays from 'common/search/FilterPlays';
-import SearchPlays from 'common/search/SearchPlays';
-import HeaderNav from './HeaderNav';
+import FilterPlays from "common/search/FilterPlays";
+import SearchPlays from "common/search/SearchPlays";
+import HeaderNav from "./HeaderNav";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import './header.css';
+import "./header.css";
 
 const Header = () => {
   const location = useLocation();
   const pathName = location.pathname;
-  
-  const [showSearch, setShowSearch] = useState(false);
-  const [headerStyle, setHeaderStyle] = useState(false);
-  const [showBrowse, setShowBrowse] = useState(false);
+
+  const [showHideBits, setShowHideBits] = useState({
+    showSearch: true,
+    showBrowse: false,
+    setHeaderStyle: true,
+  });
 
   useEffect(() => {
-    if (pathName === '/' || pathName === '/ideas') {
-      setHeaderStyle(false);
-      setShowSearch(false);
-      setShowBrowse(true);
+    if (pathName === "/") {
+      setShowHideBits({
+        ...showHideBits,
+        showSearch: false,
+        showBrowse: true,
+        setHeaderStyle: false,
+      });
+    } else if (pathName === "/ideas") {
+      setShowHideBits({
+        ...showHideBits,
+        showSearch: false,
+        showBrowse: true,
+        setHeaderStyle: true,
+      });
     } else {
-      setHeaderStyle(true);
-      setShowSearch(true);
-      setShowBrowse(false);
+      setShowHideBits({
+        ...showHideBits,
+        showSearch: true,
+        showBrowse: false,
+        setHeaderStyle: true,
+      });
     }
   }, [pathName]);
 
   return (
-    <header className={headerStyle ? "app-header" : "app-header app-header-home"}>
-        <span><Link to="/" className="app-logo"><span className="sr-only">React Play</span></Link></span>
-        <div className="app-header-search">
-        {
-          showSearch && (
-            <>                
-              <SearchPlays />
-              <FilterPlays />
-            </>
-            )
-        }
-        </div>
-        <HeaderNav showBrowse={showBrowse}/>
+    <header
+      className={
+        showHideBits.setHeaderStyle
+          ? "app-header"
+          : "app-header app-header-home"
+      }
+    >
+      <span>
+        <Link to="/" className="app-logo">
+          <span className="sr-only">React Play</span>
+        </Link>
+      </span>
+      <div className="app-header-search">
+        {showHideBits.showSearch && (
+          <>
+            <SearchPlays />
+            <FilterPlays />
+          </>
+        )}
+      </div>
+      <HeaderNav showBrowse={showHideBits.showBrowse} />
     </header>
   );
 };
-
 
 export default Header;
