63c5eabf09bce7d04336ecbddbacf430c2a8f7b8
Abhishek Khatri <abhikhatri67@gmail.com>
2022-05-08 14:18:30 +0530

    GA: Trying with location change.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import ReactGA from 'react-ga';
 
 import "App.css";
@@ -6,9 +7,12 @@ import "App.css";
 ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
 
 function App() {
-  if (!window.location.href.includes("localhost")) {
+
+  let location = useLocation();
+
+  useEffect(() =>{
     ReactGA.pageview(window.location.pathname + window.location.search);
-  }
+  }, [location]);
 
   return (
     <div className="app-body">
