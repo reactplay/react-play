898b62981c154aa4544823922cf8c46fbe18317c
Vincent <vincentbenjaminpatoc@gmail.com>
2022-05-19 13:34:20 +0800

    [plays/calendar] Add more dummy data

--- src/plays/calendar/Context.tsx
+++ src/plays/calendar/Context.tsx
@@ -1,5 +1,5 @@
-import { format } from "date-fns";
-import React, { useCallback, useState } from "react";
+import { addDays, endOfMonth, endOfWeek, format, getWeekOfMonth, isWeekend, startOfMonth, startOfWeek } from "date-fns";
+import React, { useCallback, useEffect, useState } from "react";
 import { orderBy } from 'lodash'
 
 export const Context = React.createContext<any>({
@@ -14,66 +14,83 @@ export const Context = React.createContext<any>({
 })
 
 export const ContextProvider = ({ children }: any) => {
-  const [events, setEvents] = useState<any[]>([
-    {
-      id: '1',
-      date: '2022-05-11',
-      title: 'Lorem ipsum dolor sit amet consectutar adispiscing',
-      startTime: '08:30',
-      endTime: '09:00'
-    },
-    {
-      id: '2',
-      date: '2022-05-19',
-      title: 'Lorem ipsum dolor',
-      startTime: '08:30',
-      endTime: '09:00'
-    },
-    {
-      id: '3',
-      date: '2022-05-23',
-      title: 'Lorem ipsum dolor',
-      startTime: '08:30',
-      endTime: '09:00'
-    },
-    {
-      id: '4',
-      date: '2022-05-27',
-      title: 'Lorem ipsum dolor',
-      startTime: '08:30',
-      endTime: '09:00'
-    },
-    {
-      id: '5',
-      date: '2022-05-27',
-      title: 'Lorem ipsum dolor',
-      startTime: '10:30',
-      endTime: '11:30'
-    },
-    {
-      id: '6',
-      date: '2022-05-27',
-      title: 'Lorem ipsum dolor',
-      startTime: '12:00',
-      endTime: '13:30'
-    },
-    {
-      id: '7',
-      date: '2022-05-27',
-      title: 'Lorem ipsum dolor',
-      startTime: '15:00',
-      endTime: '15:30'
-    },
-    {
-      id: '8',
-      date: '2022-05-27',
-      title: 'Lorem ipsum dolor',
-      startTime: '16:30',
-      endTime: '17:00'
-    }
-  ])
+  const [events, setEvents] = useState<any[]>([])
   const [modalTitle, setModalTitle] = useState('')
   const [modalContent, setModalContent] = useState<React.ReactNode | undefined>(undefined)
+
+  useEffect(() => {
+    // create fake events
+
+    const startDate = startOfWeek(startOfMonth(new Date()), { weekStartsOn: 0 })
+    const endDate = endOfWeek(endOfMonth(new Date()), { weekStartsOn: 0 })
+
+    let curDate = startDate
+
+    let events: any[] = []
+
+    const addEvent = (date: Date) => {
+      // add event only if date is Mon - Fri
+      if (isWeekend(date)) return
+
+      events.push({
+        id: format(date, 'yyyy-MM-dd'),
+        date: format(date, 'yyyy-MM-dd'),
+        title: 'Daily stand up',
+        startTime: '09:30',
+        endTime: '10:00'
+      })
+
+      if (getWeekOfMonth(date) % 2 === 0 && date.getDay() === 2) {
+        events.push({
+          id: format(date, 'yyyy-MM-dd') + '-key',
+          date: format(date, 'yyyy-MM-dd'),
+          title: 'Sprint Planning',
+          startTime: '15:00',
+          endTime: '17:00'
+        })
+      }
+    }
+
+    do {
+      addEvent(curDate)
+      curDate = addDays(curDate, 1)
+    } while(format(curDate, 'yyyy-MM-dd') !== format(endDate, 'yyyy-MM-dd'))
+
+    addEvent(curDate)
+
+    events = events.concat([
+      {
+        id: '1',
+        date: format(new Date(), 'yyyy-MM') + '-13',
+        title: 'Lorem ipsum dolor sit amet',
+        startTime: '07:30',
+        endTime: '08:30'
+      },
+      {
+        id: '2',
+        date: format(new Date(), 'yyyy-MM') + '-13',
+        title: 'Lorem ipsum dolor sit amet',
+        startTime: '10:00',
+        endTime: '11:00'
+      },
+      {
+        id: '3',
+        date: format(new Date(), 'yyyy-MM') + '-13',
+        title: 'Lorem ipsum dolor sit amet',
+        startTime: '12:30',
+        endTime: '13:30'
+      },
+      {
+        id: '4',
+        date: format(new Date(), 'yyyy-MM') + '-13',
+        title: 'Lorem ipsum dolor sit amet',
+        startTime: '14:00',
+        endTime: '14:30'
+      }
+    ])
+
+    setEvents(events)
+  }, [])
 
   const getEvents = useCallback((date: Date) => {
     return orderBy(events.filter(e => e.date === format(date, 'yyyy-MM-dd')), ['startTime'])
