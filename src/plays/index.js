fede2cb66d60d15af127602b478284c2a29cc98f
Programming-School <mohamedehabam99@hotmail.com>
2022-05-22 14:32:57 +0200

    Update index.js

--- src/plays/index.js
+++ src/plays/index.js
@@ -17,5 +17,4 @@ export { default as AnalogClock } from 'plays/analog-clock/AnalogClock';
 export { default as PasswordGenerator } from 'plays/password-generator/PasswordGenerator';
 export { default as WhyTypescript } from 'plays/why-typescript/WhyTypescript';
 export { default as NetlifyCardGame } from 'plays/memory-game/NetlifyCardGame';
-export { default as GitHubFinder } from 'plays/github-finder/GitHubFinder';
 //add export here
