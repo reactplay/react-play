e6ae17f4061a1d9ecc1de3a6880a324447d21e01
Vasanti Suthar <vasantisuthar15@gmail.com>
2022-05-22 22:56:39 +0530

    removed logger

--- src/plays/quizeo/src/redux/store.js
+++ src/plays/quizeo/src/redux/store.js
@@ -1,12 +1,9 @@
-import { createStore,applyMiddleware } from "redux";
+import { createStore } from "redux";
 import { persistStore } from "redux-persist";
 
-import logger from "redux-logger";
 import rootReducer from './root-reducer';
 
-const middleware = [logger];
-
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(rootReducer);
 const persistor = persistStore(store);
 
 export {store, persistor};
