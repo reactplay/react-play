5eb30b0c44aef19d80272ed9e492b6bacaf860f7
Programming-School <mohamedehabam99@hotmail.com>
2022-05-31 14:14:28 +0200

    Fixes

--- src/plays/calculator/Calculator.jsx
+++ src/plays/calculator/Calculator.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { getPlayById } from "meta/play-meta-util";
 import NumberFormat from "react-number-format";
 
@@ -111,79 +111,81 @@ function Calculator(props) {
               ultricies nisi, eget consectetur nunc nisi euismod nunc.
             </p>
           </div>
-          <div className="container">
-            <div className="wrapper">
-              <div className="screen">
-                {input !== "" || input === "0" ? (
-                  <NumberFormat
-                    value={input}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                  />
-                ) : (
-                  <NumberFormat
-                    value={preState}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                  />
-                )}
-              </div>
-              <div className="btn light-gray" onClick={reset}>
-                AC
-              </div>
-              <div className="btn light-gray" onClick={percent}>
-                %
-              </div>
-              <div className="btn light-gray" onClick={minusPlus}>
-                +/-
-              </div>
-              <div className="btn orange" onClick={operatorType}>
-                /
-              </div>
-              <div className="btn" onClick={inputNum}>
-                7
-              </div>
-              <div className="btn" onClick={inputNum}>
-                8
-              </div>
-              <div className="btn" onClick={inputNum}>
-                9
-              </div>
-              <div className="btn orange" onClick={operatorType}>
-                X
-              </div>
-              <div className="btn" onClick={inputNum}>
-                4
-              </div>
-              <div className="btn" onClick={inputNum}>
-                5
-              </div>
-              <div className="btn" onClick={inputNum}>
-                6
-              </div>
-              <div className="btn orange" onClick={operatorType}>
-                +
-              </div>
-              <div className="btn" onClick={inputNum}>
-                1
-              </div>
-              <div className="btn" onClick={inputNum}>
-                2
-              </div>
-              <div className="btn" onClick={inputNum}>
-                3
-              </div>
-              <div className="btn orange" onClick={operatorType}>
-                -
-              </div>
-              <div className="btn zero" onClick={inputNum}>
-                0
-              </div>
-              <div className="btn" onClick={inputNum}>
-                .
-              </div>
-              <div className="btn" onClick={equals}>
-                =
+          <div className="outer-div">
+            <div className="container">
+              <div className="wrapper">
+                <div className="screen">
+                  {input !== "" || input === "0" ? (
+                    <NumberFormat
+                      value={input}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  ) : (
+                    <NumberFormat
+                      value={preState}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  )}
+                </div>
+                <div className="btn light-gray" onClick={reset}>
+                  AC
+                </div>
+                <div className="btn light-gray" onClick={percent}>
+                  %
+                </div>
+                <div className="btn light-gray" onClick={minusPlus}>
+                  +/-
+                </div>
+                <div className="btn orange" onClick={operatorType}>
+                  /
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  7
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  8
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  9
+                </div>
+                <div className="btn orange" onClick={operatorType}>
+                  X
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  4
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  5
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  6
+                </div>
+                <div className="btn orange" onClick={operatorType}>
+                  +
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  1
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  2
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  3
+                </div>
+                <div className="btn orange" onClick={operatorType}>
+                  -
+                </div>
+                <div className="btn zero" onClick={inputNum}>
+                  0
+                </div>
+                <div className="btn" onClick={inputNum}>
+                  .
+                </div>
+                <div className="btn" onClick={equals}>
+                  =
+                </div>
               </div>
             </div>
           </div>
