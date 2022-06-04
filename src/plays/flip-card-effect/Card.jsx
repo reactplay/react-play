2fa88335559baedcf58144006819d1f8662218ad
Deepak8717 <sendit2deepak@gmail.com>
2022-06-04 09:28:35 +0530

    space seprating functions and css classes from one another

--- src/plays/flip-card-effect/Card.jsx
+++ src/plays/flip-card-effect/Card.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 
 function Card(item) {
   const [clicked, setClicked] = useState(false);
+
   const handleClick = () => {
     setClicked(!clicked);
   };
+
   return (
     <div className="card-container">
       <div
