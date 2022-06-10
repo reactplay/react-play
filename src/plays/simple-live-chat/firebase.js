2f8b920ede0e6a3a72a0b75f63b41e1698636ca6
Shyam Mahanta <63530626+Angryman18@users.noreply.github.com>
2022-06-10 10:34:11 +0530

    Update firebase.js

--- src/plays/simple-live-chat/firebase.js
+++ src/plays/simple-live-chat/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
