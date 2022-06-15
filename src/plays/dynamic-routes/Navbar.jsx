8c92742db0afea5a5871a5f900dfd68bdf4047f1
Deepak8717 <sendit2deepak@gmail.com>
2022-06-15 11:05:06 +0530

    fixed autogenrated css/scss files

--- src/plays/dynamic-routes/Navbar.jsx
+++ src/plays/dynamic-routes/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar(props) {
   const [mealTypes, activeMealHandler, activeMenu] = Object.values(props);
