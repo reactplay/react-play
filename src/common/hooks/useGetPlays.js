8a96f7d424c821eadfa2995072d44cc6ee7025d9
koustov <koustov@live.com>
2022-06-18 21:30:34 +0530

    replaced array with single object for request

--- src/common/hooks/useGetPlays.js
+++ src/common/hooks/useGetPlays.js
@@ -1,42 +1,36 @@
+import { useState, useEffect } from "react";
 
-
-
-import {useState, useEffect} from 'react';
-
-import { submit } from 'common/services/request';
+import { submit } from "common/services/request";
 
 const useGetPlays = () => {
   const [loading, setLoading] = useState(true);
   const [plays, setPlays] = useState([]);
   const [error, setError] = useState(null);
 
-  const fetchPlayPayload = [
-    {
-      display: "Simple fetch play",
-      name: "Fetch_Plays",
-      function: "plays",
-      write: false,
-      params: [
-        "blog",
-        "component",
-        "cover",
-        "created_at",
-        "description",
-        "featured",
-        "github",
-        "id",
-        "language",
-        { level: ["name"] },
-        "name",
-        "path",
-        { play_tags: { tag: ["name"] } },
-        "updated_at",
-        { user: ["id", "displayName", "avatarUrl"] },
-        "video",
-      ],
-    },
-  ];
-
+  const fetchPlayPayload = {
+    display: "Simple fetch play",
+    name: "Fetch_Plays",
+    function: "plays",
+    write: false,
+    params: [
+      "blog",
+      "component",
+      "cover",
+      "created_at",
+      "description",
+      "featured",
+      "github",
+      "id",
+      "language",
+      { level: ["name"] },
+      "name",
+      "path",
+      { play_tags: { tag: ["name"] } },
+      "updated_at",
+      { user: ["id", "displayName", "avatarUrl"] },
+      "video",
+    ],
+  };
   useEffect(() => {
     const fetchPlays = async () => {
       setLoading(true);
@@ -48,15 +42,11 @@ const useGetPlays = () => {
         setError(error);
       }
       setLoading(false);
-    }
+    };
     fetchPlays();
   }, []);
 
-  return [
-    loading,
-    error,
-    plays
-  ];
-}
+  return [loading, error, plays];
+};
 
 export default useGetPlays;
