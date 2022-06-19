950c052c34ff75bbab7fe94d79fd1362f69c1ae7
koustov <koustov@live.com>
2022-06-19 15:03:33 +0530

    Removed all plays

--- src/plays/quizeo/redux/directory/directory-selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-
-const selectDirectory = (state) => state.directory;
-
-export const selectDirectorySection = createSelector(
-    [selectDirectory],
-    directory => directory.sections
-)
--- src/plays/social-card/context/SocialContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Creating the context here to hold the social card data and 
-// the function to update the data.
-export const SocialContext = React.createContext({
-  social: {}, 
-  setSocial: () => {}
-});
