98a4a77ba5a2c8c5451772e7ee052ac1275ec8ee
Angryman18 <imsmahanta@gmail.com>
2022-06-20 22:21:58 +0530

    spinner added and plop template modified

--- src/common/playlists/PlayList.jsx
+++ src/common/playlists/PlayList.jsx
@@ -1,25 +1,30 @@
 import PlayThumbnail from "./PlayThumbnail";
 import { ReactComponent as ImageOops } from "images/img-oops.svg";
 import { Fragment } from "react";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "common/spinner/spinner";
 
 import "./playlist.css";
 
-const PlayList = ({key, plays}) => {
+const PlayList = ({ plays, loading }) => {
+  if (loading) {
+   return <Loader />;
+  }
 
   if (plays?.length === 0) {
     return (
-        <div className="play-not-found">
-          <ImageOops className="play-not-found-image" />
-          <p className='page-404-lead'>Play not found</p>
-          <p className='page-404-desc'>
-            Please change your search or adjust filters to find plays.
-          </p>
-        </div>
+      <div className='play-not-found'>
+        <ImageOops className='play-not-found-image' />
+        <p className='page-404-lead'>Play not found</p>
+        <p className='page-404-desc'>
+          Please change your search or adjust filters to find plays.
+        </p>
+      </div>
     );
   }
   return (
-    <Fragment key={key}>
-      <ol className="list-plays">
+    <Fragment>
+      <ol className='list-plays'>
         {plays?.map((play, index) => (
           <PlayThumbnail key={play.id} play={play} />
         ))}
