605c0c18a8ecf7055876dc28b18d39b34ce6d3ab
Shivam Katare <katare27451@gmail.com>
2022-07-06 10:59:35 +0530

    Refactor the component

--- src/plays/navbar/NavBar.jsx
+++ src/plays/navbar/NavBar.jsx
@@ -1,19 +1,14 @@
-import { getPlayById } from 'meta/play-meta-util';
-import './NavBar.css'
 import PlayHeader from 'common/playlists/PlayHeader';
+import './NavBar.css'
 import NavListItems from './navListItems';
 
 function NavBar(props) {
-  // Do not remove the below lines. 
-  // The following code is to fetch the current play from the URL
-  const { id } = props;
-  const play = getPlayById(id);
 
   // Your Code Start below.
   return (
     <>
       <div className="play-details">
-        <PlayHeader play={play} />
+        <PlayHeader play={props} />
         <div className="play-details-body">
           {/* Your Code Starts Here */}
 
