bd7945febc064bf733fa106660d20f7ab4920545
Deepak8717 <sendit2deepak@gmail.com>
2022-07-24 16:57:46 +0530

    basic styling and search UI

--- src/plays/countries-statics/CountriesStatics.jsx
+++ src/plays/countries-statics/CountriesStatics.jsx
@@ -1,8 +1,8 @@
-import PlayHeader from 'common/playlists/PlayHeader';
-import './countriesStatics.css';
+import PlayHeader from "common/playlists/PlayHeader";
+import { BsSearch } from "react-icons/bs";
+import "./countriesStatics.css";
 
 function CountriesStatics(props) {
-
   // Your Code Start below.
 
   return (
@@ -10,17 +10,20 @@ function CountriesStatics(props) {
       <div className="play-details">
         <PlayHeader play={props} />
         <div className="play-details-body">
-        {/* Your Code Starts Here */}
-          <div>
-            <h1>Play Details -  Countries-Statics</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Pellentesque euismod, urna eu tincidunt consectetur,
-              nisi nunc ultricies nisi, eget consectetur nunc nisi
-              euismod nunc.
-            </p>
+          {/* Your Code Starts Here */}
+          <div className="w-full max-w-5xl mx-auto my-4 ">
+            <div className="relative max-w-xl mx-auto ">
+              <input
+                type="text"
+                placeholder="Search...."
+                className="w-full rounded-md border-0 outline-0"
+              />
+              <button className="absolute top-1 right-1 bg-stone-300 w-10 h-8">
+                <BsSearch className="text-xl mx-auto" />
+              </button>
+            </div>
           </div>
-        {/* Your Code Ends Here */}
+          {/* Your Code Ends Here */}
         </div>
       </div>
     </>
