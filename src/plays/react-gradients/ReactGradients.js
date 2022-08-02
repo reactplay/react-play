57cabadacfebe7abbaddbd6250e86cef3cff45c6
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-08-02 21:03:48 +0530

    initial commit - created new play(react-gradients)

---
+++ src/plays/react-gradients/ReactGradients.js
@@ -0,0 +1,29 @@
+import PlayHeader from "common/playlists/PlayHeader";
+import "./styles.css";
+
+// WARNING: Do not change the entry componenet name
+function ReactGradients(props) {
+  // Your Code Start below.
+
+  return (
+    <>
+      <div className="play-details">
+        <PlayHeader play={props} />
+        <div className="play-details-body">
+          {/* Your Code Starts Here */}
+          <div>
+            <h1 className="text-4xl">Play Details - React Gradients</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque euismod, urna eu tincidunt consectetur, nisi nunc
+              ultricies nisi, eget consectetur nunc nisi euismod nunc.
+            </p>
+          </div>
+          {/* Your Code Ends Here */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ReactGradients;
---
+++ src/plays/react-gradients/Readme.md
@@ -0,0 +1,27 @@
+# React Gradients
+
+React Gradients is list of beautiful gradients which you can easily use for your application by copying the CSS/TailwindCSS code for the desired gradient.
+
+## Play Demographic
+
+- Language: js
+- Level: Intermediate
+
+## Creator Information
+
+- User: ammaaraslam
+- Gihub Link: https://github.com/ammaaraslam
+- Blog: 
+- Video: 
+
+## Implementation Details
+
+Update your implementation idea and details here
+
+## Consideration
+
+Update all considerations(if any)
+
+## Resources
+
+Update external resources(if any)
