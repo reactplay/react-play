e91ff2857d709f81a9e0997120d4872be71d7d11
Nirmal Kumar <nirmalkc@gmail.com>
2022-08-04 12:31:23 +0530

    like styling

--- src/common/components/Like/Like.jsx
+++ src/common/components/Like/Like.jsx
@@ -1,4 +1,5 @@
 import "./Like.scss";
+import { AiOutlineLike, AiFillLike } from "react-icons/ai";
 
 const Like = ({ onLikeClick, likeObj }) => {
   const {liked, number} = likeObj
@@ -8,29 +9,12 @@ const Like = ({ onLikeClick, likeObj }) => {
   };
   
   return (
-    <div className="like-container">
-      <div className="like-icon" onClick={likeClickHandler}>
-        <svg
-          viewBox="-4 -8 42 42"
-          className={`${liked ? "liked back" : "back"}`}
-        >
-          <path
-            d="M23.6 2c-3.363 0-6.258 2.736-7.599 5.594-1.342-2.858-4.237-5.594-7.601-5.594-4.637 0-8.4 3.764-8.4 8.401 0 9.433 9.516 11.906 16.001 21.232 6.13-9.268 15.999-12.1 15.999-21.232 0-4.637-3.763-8.401-8.4-8.401z"
-            stroke-width="5"
-          />
-        </svg>
-        <svg
-          viewBox="-4 -8 42 42"
-          className={`${liked ? "liked front" : "front"}`}
-        >
-          <path
-            d="M23.6 2c-3.363 0-6.258 2.736-7.599 5.594-1.342-2.858-4.237-5.594-7.601-5.594-4.637 0-8.4 3.764-8.4 8.401 0 9.433 9.516 11.906 16.001 21.232 6.13-9.268 15.999-12.1 15.999-21.232 0-4.637-3.763-8.401-8.4-8.401z"
-            stroke-width="2"
-          />
-        </svg>
-      </div>
-      <div className="like-count">{number}</div>
-    </div>
+    <button className="action counted -mr-0.5" onClick={likeClickHandler}>
+      <AiOutlineLike size='24px' className={`${liked ? "hidden" : "icon"}`}/>
+      <AiFillLike size='24px' className={`${liked ? "icon" : "hidden"}`}/>
+      {number > 0 ? (<div className="count-value">{number}</div>):(<div></div>)}
+      
+    </button>
   );
 };
 
