983a3e830d2f4cd34dc3f3f3a6d19963831f473b
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-08-04 15:55:18 +0530

    mapped each gradient to GradientComponent

--- src/plays/react-gradients/GradientComponent.js
+++ src/plays/react-gradients/GradientComponent.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const GradientComponent = () => {
-  return <div>GradientComponent</div>;
+const GradientComponent = ({ name, css, tailwind }) => {
+  return (
+    <div className="w-64 h-fit rounded-xl">
+      <div className="w-full h-56 rounded-xl" style={{ background: css }}></div>
+      <div className="w-full h-14 rounded-xl">{name}</div>
+    </div>
+  );
 };
 
 export default GradientComponent;
--- src/plays/react-gradients/ReactGradients.js
+++ src/plays/react-gradients/ReactGradients.js
@@ -1,6 +1,7 @@
 import PlayHeader from "common/playlists/PlayHeader";
 import GradientComponent from "./GradientComponent";
 import "./styles.css";
+import gradients from "./gradients.json";
 
 // WARNING: Do not change the entry componenet name
 function ReactGradients(props) {
@@ -12,8 +13,14 @@ function ReactGradients(props) {
         <PlayHeader play={props} />
         <div className="play-details-body">
           {/* Your Code Starts Here */}
-          <div>
-            <GradientComponent />
+          <div className="grid grid-cols-3 gap-10">
+            {gradients.map((gradient) => (
+              <GradientComponent
+                name={gradient.name}
+                css={gradient.css}
+                tailwind={gradient.tailwind}
+              />
+            ))}
           </div>
           {/* Your Code Ends Here */}
         </div>
