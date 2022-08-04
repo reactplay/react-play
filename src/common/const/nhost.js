7dc0c538cdb639827e9dd16781aa3f8b2e00dd84
Angryman18 <imsmahanta@gmail.com>
2022-08-04 22:01:15 +0530

    hardcoded url replaced with env var

--- src/common/const/nhost.js
+++ src/common/const/nhost.js
@@ -1,5 +1,5 @@
 const AUTH_URL = (redirectURL, provider = "github") => {
-  return `https://rgkjmwftqtbpayoyolwh.nhost.run/v1/auth/signin/provider/${provider}?redirectTo=${encodeURI(
+  return `${process.env.REACT_APP_NHOST_BACKEND_URL}/v1/auth/signin/provider/${provider}?redirectTo=${encodeURI(
     redirectURL
   )}`;
 };
