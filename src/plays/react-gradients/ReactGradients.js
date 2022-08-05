834f238e6d1d5cdfc72afa137d39d6252a2a4254
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-08-05 10:24:23 +0530

    added basic header and footer for play

--- src/plays/react-gradients/ReactGradients.js
+++ src/plays/react-gradients/ReactGradients.js
@@ -13,6 +13,9 @@ function ReactGradients(props) {
         <PlayHeader play={props} />
         <div className="play-details-body">
           {/* Your Code Starts Here */}
+          <header className="w-full h-fit py-6 px-7 flex">
+            <span>React Gradients</span>
+          </header>
           <div className="grid py-2 px-6 grid-cols-3 gap-8 place-items-center">
             {gradients.map((gradient) => (
               <GradientComponent
@@ -22,6 +25,22 @@ function ReactGradients(props) {
               />
             ))}
           </div>
+          <footer className="w-full h-fit py-4 px-6 flex justify-center items-center">
+            <span>
+              Built with 💙 by{" "}
+              <a
+                href="https://twitter.com/itsammaar_7"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Ammaar Aslam
+              </a>{" "}
+              for{" "}
+              <a href="/" target="_blank" rel="noreferrer">
+                ReactPlay.io
+              </a>
+            </span>
+          </footer>
           {/* Your Code Ends Here */}
         </div>
       </div>
