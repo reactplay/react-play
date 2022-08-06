4e73cdb1eeb7484edaca2c1d95a0f94dbc239aaf
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-08-06 19:54:10 +0530

    styled header, footer and added "add gradient" button

--- src/plays/react-gradients/ReactGradients.js
+++ src/plays/react-gradients/ReactGradients.js
@@ -2,6 +2,7 @@ import PlayHeader from "common/playlists/PlayHeader";
 import GradientComponent from "./GradientComponent";
 import "./styles.css";
 import gradients from "./gradients.json";
+import { IoColorPaletteSharp, IoAddSharp } from "react-icons/io5";
 
 // WARNING: Do not change the entry componenet name
 function ReactGradients(props) {
@@ -13,8 +14,19 @@ function ReactGradients(props) {
         <PlayHeader play={props} />
         <div className="play-details-body">
           {/* Your Code Starts Here */}
-          <div className="w-full h-fit py-6 px-7 flex">
-            <span>React Gradients</span>
+          <div className="w-full h-fit py-11 px-7 text-center">
+            <span className="font-bold text-4xl w-full text-black inline-flex justify-center items-center">
+              React
+              <IoColorPaletteSharp
+                className="text-[#00f2fe] mt-auto mb-auto"
+                size={50}
+              />
+              Gradients
+            </span>
+            <p className="mt-2 font-semibold text-xl w-full text-gray-700">
+              A collection of beautiful gradients that you can copy to easily
+              use in your application.
+            </p>
           </div>
           <div className="grid py-2 px-6 grid-cols-3 gap-8 place-items-center">
             {gradients.map((gradient, index) => (
@@ -27,18 +39,33 @@ function ReactGradients(props) {
               />
             ))}
           </div>
-          <div className="w-full h-fit py-4 px-6 flex justify-center items-center">
+          <a
+            href="https://github.com/reactplay/react-play/src/plays/react-gradients"
+            target="_blank"
+            rel="noreferrer"
+            className="fixed bottom-16 right-9 w-fit p-4 text-white bg-[#00f2fe] rounded-full font-semibold text-xl inline-flex justify-center items-center border-2 border-[#00f2fe] hover:bg-white hover:text-[#00f2fe] transition-all duration-200"
+          >
+            <IoAddSharp size={32} />{" "}
+            <span className="ml-2">Add a Gradient</span>
+          </a>
+          <div className="w-full h-fit pt-14 px-6 flex justify-center items-center">
             <span>
               Built with 💙 by{" "}
               <a
                 href="https://twitter.com/itsammaar_7"
                 target="_blank"
                 rel="noreferrer"
+                className="font-semibold underline hover:text-[#00f2fe] transition-all duration-200"
               >
                 Ammaar Aslam
               </a>{" "}
               for{" "}
-              <a href="/" target="_blank" rel="noreferrer">
+              <a
+                href="/"
+                target="_blank"
+                rel="noreferrer"
+                className="font-semibold underline hover:text-[#00f2fe] transition-all duration-200"
+              >
                 ReactPlay.io
               </a>
             </span>
