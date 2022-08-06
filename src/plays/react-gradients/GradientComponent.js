05c16db0067dac56a0297550bdb101e33d813c60
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-08-06 12:41:45 +0530

    styled GradientComponent

--- src/plays/react-gradients/GradientComponent.js
+++ src/plays/react-gradients/GradientComponent.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { SiTailwindcss, SiCss3 } from "react-icons/si";
 
 const GradientComponent = ({ name, css, tailwind }) => {
   return (
     <div
-      class="relative h-64 w-80 rounded-xl transition-all duration-150 hover:scale-105"
+      className="group relative h-72 w-[22rem] rounded-2xl transition-all duration-150 hover:scale-105 border-[6px] border-[#010326]"
       style={{ background: css }}
     >
-      <div class="absolute right-4 top-4 w-0 rounded-xl bg-[#010326] py-1 px-3 text-center transition-all duration-150 hover:w-32">
-        <span>x</span>
+      <div className="absolute group-hover:flex hidden right-0 top-0 w-fit rounded-lg rounded-tl-none rounded-br-none bg-[#010326] transition-all duration-200 p-1 justify-center items-center border-2 border-[#010326]">
+        <GradientCopyButton>
+          <SiTailwindcss />
+        </GradientCopyButton>
+        <GradientCopyButton>
+          <SiCss3 />
+        </GradientCopyButton>
+        <GradientCopyButton>
+          <span className="text-sm">#010326</span>
+        </GradientCopyButton>
+        <GradientCopyButton>
+          <span className="text-sm">#010326</span>
+        </GradientCopyButton>
       </div>
-      <div class="absolute bottom-0 w-full rounded-b-xl bg-[#010326] py-5 px-6 text-lg font-bold text-black">
+      <div class="absolute bottom-0 w-full rounded-b-lg bg-[#010326] py-4 px-6 text-xl font-bold text-white">
         {name}
       </div>
     </div>
@@ -17,3 +29,14 @@ const GradientComponent = ({ name, css, tailwind }) => {
 };
 
 export default GradientComponent;
+
+const GradientCopyButton = ({ children, handleOnClick }) => {
+  return (
+    <button
+      className="flex justify-center items-center text-center mx-1 p-2 text-gray-300 hover:text-white hover:bg-cyan-700 text-xl hover:bg-opacity-60 rounded-full transition-all duration-200"
+      onClick={handleOnClick}
+    >
+      {children}
+    </button>
+  );
+};
