8c2c0a5a6aa3bedd49e18f19cb1d26d3a7315054
Angryman18 <imsmahanta@gmail.com>
2022-08-06 20:53:00 +0530

    disable service worker on prod

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,7 +1,7 @@
 import { Workbox } from "workbox-window";
 
 const register = () => {
-  if (process.env.NODE_ENV !== "production") return;
+  if (process.env.NODE_ENV === "production") return;
   if (navigator?.serviceWorker) {
     const wb = new Workbox(`${process.env.PUBLIC_URL}/service-worker.js`);
 
