ceb44207ce72baf61b13207addbb4c405927c7aa
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-08-20 13:18:20 +0530

    adding one test case to trigger workflow

--- cypress/e2e/homePage.cy.ts
+++ cypress/e2e/homePage.cy.ts
@@ -7,17 +7,21 @@ describe("Test home page", () => {
     cy.visit("/");
   });
 
-  it("Header component should render properly", () => {
+  it.only("Header component should render properly", () => {
     cy.get('[data-testid="app-header"]').should("be.visible");
     cy.get('[data-testid="app-logo"]').should("be.visible");
     cy.get('[data-testid="plays-search-box-container"]').should("not.exist");
     cy.get('[data-testid="header-links-container"]').should("be.visible");
-    cy.get('[data-testid="browse-btn"]').should("be.visible");
+    cy.get('[data-testid="browse-btn"]').should("be.visible").as("browseBtn");
     cy.get('[data-testid="create-btn"]').should("be.visible");
     cy.get('[data-testid="ideas-btn"]').should("be.visible");
     cy.get('[data-testid="github-btn"]').should("be.visible");
     cy.get('[data-testid="twitter-btn"]').should("be.visible");
     cy.get('[data-testid="share-btn"]').should("be.visible");
+
+    cy.get("@browseBtn").click();
+    cy.get('[data-testid="plays-search-box-container"]').should("be.visible");
+
   });
 
   it("Tweet section should render with all tweets", () => {
