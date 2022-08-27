0fb248d420f40ff8f1c08466056c2e4c35ce7330
Deepak8717 <sendit2deepak@gmail.com>
2022-08-27 08:46:00 +0530

    added the auto search functionality

--- src/plays/countries-statics/CountriesStatics.jsx
+++ src/plays/countries-statics/CountriesStatics.jsx
@@ -1,12 +1,71 @@
 import PlayHeader from "common/playlists/PlayHeader";
 import Map from "./Map";
 import Country from "./Country";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchAndFilter from "./SearchAndFilter";
-
+import { GeoContext } from "./Context";
+import MapData from "./featues.json";
 function CountriesStatics(props) {
   // Your Code Start below.
   const [activeGeo, setActiveGeo] = useState("ind");
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [selected, setSelected] = useState("");
+  const [searchQuery, setSearchQuery] = useState("India");
+  //search query return the first 10 matching results.
+  const SearchResult = MapData.objects.world.geometries
+    .filter((o) =>
+      o.properties.name.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    .slice(0, 10);
+
+  const keyPressHandler = (e) => {
+    const length = SearchResult.length;
+    //downArrow Key
+    if (e.keyCode === 40) {
+      setIndex(index + 1);
+      if (index >= length - 1) {
+        setIndex(0);
+      }
+    }
+    //upArrow Key
+    if (e.keyCode === 38) {
+      setIndex(index - 1);
+      if (index <= 0) {
+        setIndex(length - 1);
+      }
+    }
+    //esc key
+    if (e.keyCode === 27) {
+      setShowSuggestions(false);
+      e.currentTarget.blur();
+    }
+    //Enter key
+    if (e.keyCode === 13) {
+      if (searchQuery !== null) {
+        setShowSuggestions(false);
+      }
+    }
+  };
+  useEffect(() => {
+    setSelected(SearchResult[index].properties.name);
+  }, [index]);
+
+  const handleOnchange = (e) => {
+    setShowSuggestions(true);
+    setSearchQuery(e.target.value);
+    setSelected(e.target.value);
+  };
+  const handleOnFocus = () => {
+    setShowSuggestions(true);
+  };
+  const handleOnBlur = () => {
+    setShowSuggestions(false);
+  };
+  const searchResultClickHandler = (i) => {
+    setIndex(i);
+    setShowSuggestions(false);
+  };
   const handleClickMap = (geo) => {
     setActiveGeo(geo.id.toLowerCase());
   };
@@ -22,11 +81,26 @@ function CountriesStatics(props) {
           <h2 className="text-xl text-center capitalize">
             Learn geography in fun and interactive way
           </h2>
-          <SearchAndFilter />
-          <div className="flex flex-col xl:flex-row justify-between">
-            <Map activeGeo={activeGeo} handleClickMap={handleClickMap} />
-            <Country activeGeo={activeGeo} />
-          </div>
+          <GeoContext.Provider
+            value={{
+              showSuggestions,
+              index,
+              selected,
+              searchQuery,
+              SearchResult,
+              keyPressHandler,
+              handleOnchange,
+              handleOnFocus,
+              handleOnBlur,
+              searchResultClickHandler,
+            }}
+          >
+            <SearchAndFilter />
+            <div className="flex flex-col xl:flex-row justify-between">
+              <Map activeGeo={activeGeo} handleClickMap={handleClickMap} />
+              <Country activeGeo={activeGeo} />
+            </div>
+          </GeoContext.Provider>
 
           {/* Your Code Ends Here */}
         </div>
--- src/plays/countries-statics/SearchAndFilter.jsx
+++ src/plays/countries-statics/SearchAndFilter.jsx
@@ -1,50 +1,58 @@
-import React, { useState } from 'react' 
+import React, { useContext } from "react";
 import { BsSearch } from "react-icons/bs";
-import "./countriesStatistic.css"
-import MapData  from "./featues.json"
+import "./countriesStatistic.css";
+
+import { GeoContext } from "./Context";
 
 const SearchAndFilter = () => {
-  const [showSuggestions, setShowSuggestions] = useState(false)
-  const [searchQuery, setSearchQuery] = useState("");
-  const SearchResult = MapData.objects.world.geometries.filter(o=>o.properties.name.toLowerCase().includes(searchQuery.toLowerCase())).slice(0, 10)
-  const handleOnKeyDown = (e)=>{
-    if(e.keyCode === 27){
-      setShowSuggestions(false)
-      e.currentTarget.blur()
-    }
-  }
-  const handleOnchange=(e)=>{
-    setSearchQuery(e.target.value)
-  }
-  const handleOnFocus=()=>{
-   setShowSuggestions(true)
-  }
-  const handleOnBlur =()=>{
-    setShowSuggestions(false)
-  }
+  const {
+    showSuggestions,
+    index,
+    selected,
+    SearchResult,
+    keyPressHandler,
+    handleOnchange,
+    handleOnFocus,
+    searchResultClickHandler,
+  } = useContext(GeoContext);
   return (
-    <div className="w-full max-w-[550px] mx-auto flex justify-center" >
-      <div className='relative w-full'>
-       <input type="text" className='geo-now-searchbar w-full rounded-lg bg-inherit' placeholder='Enter the country...' 
-       value={searchQuery}
-       onChange={(e)=>handleOnchange(e)}
-       onFocus={handleOnFocus}
-       onBlur={handleOnBlur}
-       onKeyDown={(e)=>handleOnKeyDown(e)}
-       /> 
-       <div className='absolute top-[50%] translate-y-[-50%] right-0  w-[30px] h-[30px] mx-2 bg-cyan-500 cursor-pointer'>
-        <BsSearch className='absolute top-1 left-1 text-2xl text-white' /></div>
-        {showSuggestions && 
-        <div className='absolute w-full bg-white p-4'>
-          {SearchResult.map((country)=>{
-            return <div className='border-b cursor-pointer hover:bg-cyan-100 font-bold'>{country.properties.name}</div>
-          })}
+    <div className="w-full max-w-[550px] mx-auto flex justify-center">
+      <div className="relative w-full">
+        <input
+          type="text"
+          className="geo-now-searchbar w-full rounded-lg bg-inherit"
+          placeholder="Enter the country..."
+          value={selected}
+          onChange={(e) => handleOnchange(e)}
+          onFocus={handleOnFocus}
+          onKeyDown={keyPressHandler}
+        />
+        <div className="absolute top-[50%] translate-y-[-50%] right-0  w-[30px] h-[30px] mx-2 bg-cyan-500 cursor-pointer">
+          <BsSearch className="absolute top-1 left-1 text-2xl text-white" />
         </div>
-          }
+        {showSuggestions && (
+          <div
+            className="absolute w-full bg-white p-4 "
+            onKeyDown={keyPressHandler}
+          >
+            {SearchResult.map((country, i) => {
+              return (
+                <div
+                  key={i}
+                  onClick={() => searchResultClickHandler(i)}
+                  className={`border-b cursor-pointer hover:bg-cyan-100 font-bold ${
+                    index === i ? "geo-search-selected" : ""
+                  }`}
+                >
+                  {country.properties.name}
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default SearchAndFilter
+export default SearchAndFilter;
