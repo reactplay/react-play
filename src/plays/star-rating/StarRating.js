e1e9db247d3451dda118c88b19f03629d3f3ece5
frankiefab100 <46662771+frankiefab100@users.noreply.github.com>
2022-08-27 15:10:47 +0100

    update star-rating play

--- src/plays/star-rating/StarRating.js
+++ src/plays/star-rating/StarRating.js
@@ -10,6 +10,24 @@ function StarRating(props) {
   const [rating, setRating] = useState(null);
   const [hoverFill, setHoverFill] = useState(null);
 
+  const getReviewLabel = (rating) => {
+    switch (rating) {
+      case 1:
+        return `Very bad ${String.fromCodePoint("0x1F922")}`;
+      case 2:
+        return `Bad ${String.fromCodePoint("0x1F97A")}`;
+      case 3:
+        return `Okay ${String.fromCodePoint("0x1F60C")}`;
+      case 4:
+        return `Good ${String.fromCodePoint("0x1F60A")}`;
+      case 5:
+        return `Excellent ${String.fromCodePoint("0x1F929")}`;
+
+      default:
+        return "";
+    }
+  };
+
   return (
     <>
       <div className="play-details">
@@ -18,6 +36,14 @@ function StarRating(props) {
           {/* Your Code Starts Here */}
 
           <div className="star-wrapper">
+            <h2 className="review-text">
+              {rating
+                ? getReviewLabel(rating)
+                : hoverFill
+                ? getReviewLabel(hoverFill)
+                : null}
+            </h2>
+
             <div className="star">
               {[...Array(5)].map((_, index) => {
                 const ratingValue = index + 1;
@@ -30,7 +56,8 @@ function StarRating(props) {
                     onClick={() => setRating(ratingValue)}
                   >
                     <FaStar
-                      size={100}
+                      className="FaStar"
+                      size={80}
                       style={{
                         color:
                           ratingValue <= (hoverFill || rating)
