8a5aac2e5ddce7c28f3d9f3ac4cfb9257f0e56bc
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-08-30 14:36:34 +0530

    Fix 🐛 Bug #479: The Console has bunch of warnings (#480)
    * Fix 🐛 Bug #479: The Console has bunch of warnings
    
    * Addressing review comments
    
    * Fix spacing issue
    
    * Remove `quoteArray` from dependency
    
    Co-authored-by: Tapas Adhikary <tapas.adhikary@gmail.com>

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 
-const Footer = () => {  return (
-    <footer className='app-footer text-center'>
-      <hr className='separater' />
-      <div className='flex justify-center items-center py-2'>
-        <Link to='/tech-stacks'>
+const Footer = () => {
+  return (
+    <footer className="app-footer text-center">
+      <hr className="separater" />
+      <div className="flex justify-center items-center py-2">
+        <Link to="/tech-stacks">
           ReactPlay - The MIT License (MIT) Copyright &copy;
           {new Date().getFullYear()} . Powered by{" "}
-          <a className='text-link-default'>Top Notch Tech Stacks</a>.
+          <a href="/" className="text-link-default">
+            Top Notch Tech Stacks
+          </a>
+          .
         </Link>
       </div>
     </footer>
