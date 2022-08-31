dbddc201f705433953ecb9a192f0e085e6811329
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-08-31 16:10:44 +0530

    refactoring

--- src/plays/pokemon-stats/components/Pokemoncard.jsx
+++ src/plays/pokemon-stats/components/Pokemoncard.jsx
@@ -1,55 +1,41 @@
-import React from 'react'
-import './pokecard.css'
+import React from "react";
+import "./pokecard.css";
 const Pokemoncard = (props) => {
-  const {pokemon} =props;
+  const { pokemon } = props;
   return (
-    <div className='cont'> 
-      {/* <div className='Pokemoncard'>
-        <h1>{pokemon.species.name}</h1>
-        <div className='imageinfo'>
-          <img src={pokemon.sprites.front_default} alt='' />
-        </div>
-        <div className='pokemondetails'>
-            <h3>HP: {pokemon.stats[0].base_stat}</h3>
-            <h3>Attack: {pokemon.stats[1].base_stat}</h3>
-            <h3>Defense: {pokemon.stats[2].base_stat}</h3>
-            <h3>Type: {pokemon.types[0].type.name}</h3>
-        </div>
-      </div> */}
+    <div className="cont">
       <div class="wrapper">
-    <div class="clash-card barbarian">
-      <div class="clash-card__image clash-card__image--barbarian">
-        <img src={pokemon.sprites.front_default} alt="barbarian"/>
+        <div class="clash-card barbarian">
+          <div class="clash-card__image clash-card__image--barbarian">
+            <img src={pokemon.sprites.front_default} alt="barbarian" />
+          </div>
+          <div class="name">{pokemon.species.name}</div>
+
+          <div class="powers">
+            <div class="one-third">
+              <div class="stat-value">HP:</div>
+              <div class="stat"> {pokemon.stats[0].base_stat} </div>
+            </div>
+
+            <div class="one-third">
+              <div class="stat-value">ATTACK:</div>
+              <div class="stat">{pokemon.stats[1].base_stat}</div>
+            </div>
+
+            <div class="one-third no-border">
+              <div class="stat-value">DEFENSE:</div>
+              <div class="stat">{pokemon.stats[2].base_stat}</div>
+            </div>
+
+            <div class="one-third no-border">
+              <div class="stat-value">TYPE:</div>
+              <div class="stat">{pokemon.types[0].type.name}</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="name">{pokemon.species.name}</div>
-
-      <div class="powers">
-        <div class="one-third">
-          <div class="stat-value">HP:</div>
-          <div class="stat"> {pokemon.stats[0].base_stat} </div>
-        </div>
-
-        <div class="one-third">
-          <div class="stat-value">ATTACK:</div>
-          <div class="stat">{pokemon.stats[1].base_stat}</div>
-        </div>
-
-        <div class="one-third no-border">
-          <div class="stat-value">DEFENSE:</div>   
-          <div class="stat">{pokemon.stats[2].base_stat}</div>
-        </div>
-
-        <div class="one-third no-border">
-          <div class="stat-value">TYPE:</div>   
-          <div class="stat">{pokemon.types[0].type.name}</div>
-        </div>
-
-      </div>
-
     </div>
-  </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Pokemoncard
+export default Pokemoncard;
--- src/plays/pokemon-stats/components/Search.jsx
+++ src/plays/pokemon-stats/components/Search.jsx
@@ -2,40 +2,25 @@ import React from "react";
 import "./search.css";
 // import pokeball from '../images/pokeball.png';
 const search = (props) => {
-  const {setname , Pokemon} = props;
+  const { setname, Pokemon } = props;
   return (
-    // <div class="form-container sign-in-container">
-    //   <div className="imager">
-    //   <h1 className="heading">Pokemon Info</h1>
-    //   <img src="" alt="Pokeball" className="pokeball"/>
-    //   </div>
-    //   <div class="demo-flex-spacer"></div>
-
-    //   <div class="webflow-style-input">
-    //     <input  type="text" placeholder="Pokemon"  onChange={(e) => setname(e.target.value)}></input>
-    //   </div>
-
-    //   <div class="demo-flex-spacer"></div>
-    //   <button className="button-56" onClick={Pokemon}>Search</button>
-    // </div>
-
     <div className="main">
-
-<form>
-    
-    <div class="group">      
-      <input type="text" required />
-      <span class="highlight"></span>
-      <span class="bar"></span>
-      <label>Name</label>
+      <div class="form__group field">
+        <input
+          type="input"
+          class="form__field"
+          placeholder="Name"
+          name="name"
+          id="name"
+          required
+          onChange={(e) => setname(e.target.value)}
+        />
+        <label for="name" class="form__label">
+          Name
+        </label>
+      </div>
+      <button className="button-30" onClick={Pokemon} >Search</button>
     </div>
-      
-  
-    
-  </form>
-
-    </div>
-
   );
 };
 
