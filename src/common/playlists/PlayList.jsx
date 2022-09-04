50ec38c97b91d4c79383defd3368217b1d006ff0
Vasanti Suthar <vasantisuthar15@gmail.com>
2022-09-04 23:44:22 +0530

    updated response

--- src/common/playlists/PlayList.jsx
+++ src/common/playlists/PlayList.jsx
@@ -35,8 +35,7 @@ const PlayList = () => {
         <ImageOops className='play-not-found-image' />  
         <p className='page-404-lead'>Play not found 
         {hasFilterQuery? " for matched filter criteria" : null}
-        {hasFilterQuery && searchTerm ? " and": null}
-        {searchTerm ? ` for  ${searchTerm}` : null}
+        {searchTerm ? " for " + searchTerm : null}
         </p>
         <p className='page-404-desc'>
           Please change your search or adjust filters to find plays.
