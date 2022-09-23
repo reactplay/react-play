9ece0ad964241bd20842aa9c361dbe3cd211a271
Nirmal Kumar <nirmalkc@gmail.com>
2022-09-23 19:06:03 +0530

    hackathon banner (#561)
    * hackathon banner
    
    * logo fix
    
    * fixing banner issues
    
    * logo fix with tag line
    
    Co-authored-by: Tapas Adhikary <tapas.adhikary@gmail.com>

--- src/common/activities/hackathon/HackathonBanner.jsx
+++ src/common/activities/hackathon/HackathonBanner.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { AiFillGithub } from "react-icons/ai";
+import { BsGithub } from "react-icons/bs";
 import { FiStar } from "react-icons/fi";
 import { IoIosRocket } from "react-icons/io";
-import HackerBannerImage from "./banner.png";
+import HackerBannerImage from "./img-hero-coders.png";
+import HRPLogo from "./Logo.png";
 import useFetch from "common/hooks/useFetch";
 import "./hackathonBanner.css";
 
@@ -12,52 +14,48 @@ function HackathonBanner() {
   );
   return (
     <div className="flex flex-col lg:flex-row justify-between w-[98%]  max-w-[600px]  md:max-w-[992px] lg:max-w-[1200px] mt-16 m-auto text-white">
-      <div className="w-[90%] lg:w-[60%] mx-auto text-center lg:text-left">
-        <h1 className="inline-block text-4xl md:text-5xl lg:text-7xl font-bold">
-          ReactPlay
+      <div className="w-[100%] lg:w-[60%] text-center lg:text-left">
+        <span className="text-lg md:text-xl lg:text-2xl">ReactPlay Presents</span>
+        <h1 className=" uppercase text-5xl md:text-6xl lg:text-8xl font-bold text-cyan-300 text-center lg:text-left">
+          <span className="sr-only">HACK-R-PLAY</span>
+          <img src={HRPLogo} className="w-64 lg:w-auto ml-auto mr-auto lg:ml-0 lg:mr-0 lg:inline-block" />
         </h1>
-        <span className="text-lg md:text-xl lg:text-2xl">Presents</span>
-        <h1 className=" uppercase text-5xl md:text-6xl lg:text-8xl font-bold text-cyan-300">
-          HACK-R-PLAY
-        </h1>
-        <span className="text-xl md:text-2xl  text-cyan-300">
-          20th Sep - 20th Oct 2022
-        </span>
         <div className="my-2 md:my-4 md:text-xl">
-          <p className="mb-2">Developers and Hacking are inseparable!</p>
-          <p>
+          <p className="text-2xl font-bold mt-8 mb-4 text-center lg:text-left">Developers and Hacking are inseparable!</p>
+          <p className="text-base opacity-50 mt-4 mb-8 text-center lg:text-left">
             ReactPlay brings you the opportunity to take part in the Hackathon
             and learn from it. Showcase your mindblowing ideas, build projects,
             and create content - there are also chances to win exciting prizes.
           </p>
         </div>
-        <div className="flex flex-col sm:flex-row justify-center lg:justify-start w-full ">
-          <a
-            href="https://hustles.reactplay.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="p-4 lg:w-48 leading-10 my-2 rounded-full bg-cyan-300 border-2 border-cyan-300 hover:bg-slate-300 hover:border-slate-300 text-black text-center text-lg font-bold  cursor-pointer"
-          >
-            <span>
-              <IoIosRocket className="inline-block w-8 h-8" />
-              Get started
-            </span>
-          </a>
-          <a
+        <div className="">
+          <div className="body-c2a body-c2a-hackathon">
+            <a
+              href="https://hustles.reactplay.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="body-c2a-btn  body-c2a-btn--primary"
+            >
+              <IoIosRocket className="icon"/>
+              <span className="btn-label">Get started</span>
+            </a>
+            <a
             href="https://github.com/reactplay/activities"
             target="_blank"
             rel="noopener noreferrer"
-            className="p-4 my-2 mx-2 rounded-full border-2 border-cyan-300 text-white font-bold cursor-pointer hover:bg-slate-300 hover:border-slate-300 group group hover:text-black "
+            className="body-c2a-btn umami--click--github"
           >
-            <AiFillGithub className="inline-block w-6 h-6 mx-2 text-cyan-300 group-hover:text-black" />
-            GitHub
-            <div className="mx-2 inline-block">
-              <FiStar className="inline-block  w-8 text-xl" />
-              <div className="inline-block text-sm w-8 leading-9">
-                {data.stargazers_count}
-              </div>
-            </div>
+            <BsGithub className="icon" />
+            <span className="btn-label">
+              GitHub{" "}
+              <div className="label-info-more">
+                <FiStar />{" "}
+                <div className="more-label">{data.stargazers_count}</div>
+              </div>{" "}
+            </span>
           </a>
+          </div>
+          
         </div>
       </div>
       <div>
