91fd48ad186992f8bead20fed3f7371b811e2aa8
Makdoom Shaikh <makshaikh99@gmail.com>
2022-10-05 21:44:09 +0530

    Implemented emojicard & working on search emoji

--- src/plays/emojipedia/Emojipedia.js
+++ src/plays/emojipedia/Emojipedia.js
@@ -17,6 +17,23 @@ function Emojipedia(props) {
   // Fetch all the emojis
   const { data, loading, error } = useFetch(URL);
 
+  const handleEmojiSearch = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    setEmojisList(
+      emojisList?.filter((item) => {
+        if (value == "") {
+          return item;
+        } else if (
+          item?.unicodeName.toLowerCase().includes(value.toLowerCase())
+        ) {
+          return item;
+        }
+      })
+    );
+    // setEmojisList(emojisList?.filter((item) => item?.unicodeName === value));
+  };
+
   useEffect(() => {
     if (data?.length) {
       setEmojisList(data);
@@ -30,29 +47,26 @@ function Emojipedia(props) {
         <div className="play-details-body !p-2 bg-gradient-to-r from-[#0f172a] to-[#1c2d52]">
           {/* Your Code Starts Here */}
           <div className="emojipedia__container max-w-[95%] mx-auto text-center ">
-            <div className="max-w-[700px] mx-auto m-4  ">
-              <div className="w-[95%] flex justify-center items-center bg-[#3d4b63] rounded-md px-3 text-gray-300">
+            <div className="max-w-[700px] mx-auto m-4 mb-12 ">
+              <div className="w-[100%] mx-auto flex justify-center items-center bg-[#3d4b63] rounded-md px-3 text-gray-300">
                 <FiSearch fontSize={22} />
                 <input
                   type="text"
                   placeholder="Search Emoji..."
+                  onChange={handleEmojiSearch}
                   className="w-[100%] !p-3 outline-none !border-0 bg-transparent text-gray-300"
                 />
               </div>
             </div>
 
-            <div className="text-white grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+            <div className="text-white grid grid-cols-2 gap-8 md:grid-cols-4 lg:grid-cols-6">
               {loading ? (
                 <p>loading</p>
-              ) : emojisList?.length ? (
+              ) : (
+                emojisList?.length &&
                 emojisList?.map((emoji, index) => (
                   <EmojiCard key={index} emoji={emoji} />
                 ))
-              ) : (
-                <p>
-                  Oops..! unable to load the emojis for you, please refresh the
-                  page
-                </p>
               )}
             </div>
           </div>
--- src/plays/emojipedia/components/EmojiCard.js
+++ src/plays/emojipedia/components/EmojiCard.js
@@ -1,8 +1,20 @@
-
-const EmojiCard = ({emoji}) => {
+const EmojiCard = ({ emoji }) => {
+  // {
+  //     "slug": "grinning-face",
+  //     "character": "😀",
+  //     "unicodeName": "grinning face",
+  //     "codePoint": "1F600",
+  //     "group": "smileys-emotion",
+  //     "subGroup": "face-smiling"
+  // }
   return (
-    <div>{emoji?.slug}</div>
-  )
-}
+    <div className="bg-[#333f53] py-7 px-4 rounded-lg cursor-pointer transform transition duration-200 hover:scale-110 hover:shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]">
+      <p className="text-5xl mb-5">
+        {String.fromCodePoint(parseInt(emoji?.codePoint, 16))}
+      </p>
+      <span className="text-[14px]">{emoji?.unicodeName}</span>
+    </div>
+  );
+};
 
-export default EmojiCard
+export default EmojiCard;
