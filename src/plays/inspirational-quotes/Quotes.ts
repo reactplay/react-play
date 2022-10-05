bb690aacf7fce9d51810fa1acbdcc3a0d93a5f01
Muzaffar <muzaffar640@gmail.com>
2022-10-05 13:09:58 +0530

    update quote list

--- src/plays/inspirational-quotes/Quotes.ts
+++ src/plays/inspirational-quotes/Quotes.ts
@@ -443,11 +443,6 @@ const quotes: Array<Quote> = [
     author: "Arthur Ashe",
   },
   {
-    quote:
-      "When I was 5 years old, my mother always told me that happiness was the key to life.  When I went to school, they asked me what I wanted to be when I grew up.  I wrote down ‘happy’.  They told me I didn’t understand the assignment, and I told them they didn’t understand life.",
-    author: "John Lennon",
-  },
-  {
     quote: "Fall seven times and stand up eight.",
     author: "Japanese Proverb",
   },
