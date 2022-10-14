a71b0f63100c14fa82b2aadb06f8ba13453854fd
Shivam-Katare <katare27451@gmail.com>
2022-10-15 00:14:06 +0530

    Added the cells for the game board

---
+++ src/plays/2048/Game2048Components/Cell.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cell(props) {
+  return (
+    <div className={"cell cell--" + props.label}>
+      <span className="cell__label">{props.label}</span>
+    </div>
+  );
+}
+
+export default Cell;
