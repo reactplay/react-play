12860f5eeb9832e7a7cbd200d3dd520e538f2856
senali <senalidilumika@gmail.com>
2022-10-14 22:56:11 +0530

    fix: remove button disabled condition

--- src/plays/personal-profile-card/components/profile-form.tsx
+++ src/plays/personal-profile-card/components/profile-form.tsx
@@ -125,7 +125,6 @@ const ProfileForm = ({
         <button
           type="button"
           className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none"
-          disabled={profile === null && true}
           onClick={onClear}
         >
           Clear
