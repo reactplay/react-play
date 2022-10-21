51d94abf76e8752b6a70d00cb2919cd3016d4e81
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-10-21 23:49:25 +0530

    Update src/plays/sierpinski-triangle/utils/index.ts
    Co-authored-by: Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>

--- src/plays/sierpinski-triangle/utils/index.ts
+++ src/plays/sierpinski-triangle/utils/index.ts
@@ -1,8 +1,7 @@
 import P5 from "p5";
 
-export const generateRandomColor = (p: P5): P5.Color => {
-  return p.color(p.random(0, 250), p.random(0, 250), p.random(0, 250));
-};
+export const generateRandomColor = (p: P5): P5.Color => p.color(p.random(0, 250), p.random(0, 250), p.random(0, 250));
+
 export const getMidPoint = (p: P5, p1: P5.Vector, p2: P5.Vector): P5.Vector => {
   return p.createVector((p1?.x + p2?.x) / 2, (p1?.y + p2?.y) / 2);
 };
