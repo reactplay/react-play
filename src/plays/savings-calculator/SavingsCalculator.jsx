92c93d664ba27dc4d115f03fe9257929b41d36a2
Keerthivasan D <keerthi.vasan.d@gmail.com>
2022-10-21 14:19:12 +0530

    Initial file setup with images

---
+++ src/plays/savings-calculator/SavingsCalculator.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./savingsCalculator.scss"
+
+function SavingsCalculator() {
+  return (
+    <div>SavingsCalculator</div>
+  )
+}
+
+export default SavingsCalculator
