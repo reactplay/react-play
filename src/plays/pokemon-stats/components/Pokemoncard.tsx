85c2439d35b38c537bcb0cad1379e8f8d147dae6
Gaurav Joshi <gj451026@gmail.com>
2022-10-21 22:03:02 +0530

    Data visualization added in pokemon-stats

--- src/plays/pokemon-stats/components/Pokemoncard.tsx
+++ src/plays/pokemon-stats/components/Pokemoncard.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 import { Props } from "../types";
 import "./pokecard.css";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Radar } from 'react-chartjs-2';
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+
 const Pokemoncard = (props: Props) => {
   const { pokemon } = props;
+  let pokemontype = pokemon.types[0].type.name + " type"
+  const data = {
+    labels: ['HP', 'ATTACK','SPECIAL-ATTACK',"SPEED", "DEFENSE",],
+    datasets: [
+      {
+        label: pokemontype,
+        data: [60, pokemon.stats[1].base_stat, pokemon.stats[3].base_stat, pokemon.stats[5].base_stat, pokemon.stats[2].base_stat,],
+        backgroundColor: 'rgba(155, 99, 132, 0.2)',
+        borderColor: 'rgba(5, 99, 132, 1)',
+        borderWidth: 2,
+        pointBorderWidth: 0,
+      },
+    ],
+  };
+
+  
   return (
     <div className="poke-card-cont">
       <div className="wrapper">
@@ -13,25 +50,26 @@ const Pokemoncard = (props: Props) => {
           <div className="poke-name">{pokemon.species.name}</div>
 
           <div className="pokemon-stats">
-            <div className="one-third">
+            {/* <div className="one-third">
               <div className="stat">HP:</div>
-              <div className="stat-value"> {pokemon.stats[0].base_stat} </div>
+              <div className="stat-value">  </div>
             </div>
 
             <div className="one-third">
               <div className="stat">ATTACK:</div>
-              <div className="stat-value">{pokemon.stats[1].base_stat}</div>
+              <div className="stat-value">{}</div>
             </div>
 
             <div className="one-third no-border">
               <div className="stat">DEFENSE:</div>
-              <div className="stat-value">{pokemon.stats[2].base_stat}</div>
+              <div className="stat-value">{}</div>
             </div>
 
             <div className="one-third no-border">
               <div className="stat">TYPE:</div>
-              <div className="stat-value">{pokemon.types[0].type.name}</div>
-            </div>
+              <div className="stat-value"></div>
+            </div> */}
+            <Radar data={data}  options={{color:"black"}}/>
           </div>
         </div>
       </div>
