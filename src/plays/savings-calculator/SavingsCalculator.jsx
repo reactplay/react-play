bcfe3c837c1377083a9ba122b499cfb8c2676367
Keerthivasan D <keerthi.vasan.d@gmail.com>
2022-10-27 13:49:30 +0530

    Add play's header to play

--- src/plays/savings-calculator/SavingsCalculator.jsx
+++ src/plays/savings-calculator/SavingsCalculator.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
+import PlayHeader from "common/playlists/PlayHeader";
 import Total from "./components/Total";
 import SavingOptions from "./components/SavingOptions";
 import "./styles/savingsCalculator.scss";
 
-function SavingsCalculator() {
+function SavingsCalculator(props) {
   const [total, setTotal] = useState(2327);
   const [currency, setCurrency] = useState("INR");
 
   return (
-    <div className="savingsCalculator__container">
-      <div className="savingsCalculator">
-        <Total total={total} currency={currency} />
-        <SavingOptions
-          currency={currency}
-          setCurrency={setCurrency}
-          setTotal={setTotal}
-        />
+    <>
+      <div className="play-details">
+        <PlayHeader play={props} />
+        <div className="savingsCalculator__container">
+          <div className="savingsCalculator">
+            <Total total={total} currency={currency} />
+            <SavingOptions
+              currency={currency}
+              setCurrency={setCurrency}
+              setTotal={setTotal}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
