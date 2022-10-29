64dca03149ed8ed601b1de981654bd69fd592798
Shivam-Katare <katare27451@gmail.com>
2022-10-29 08:50:48 +0530

    Format the file

--- src/plays/2048/Game2048Components/Cell.js
+++ src/plays/2048/Game2048Components/Cell.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Cell(props) {
   return (
-    <div className={"cell cell--" + props.label}>
+    <div className={`cell cell--${props.label}`}>
       <span className="cell__label">{props.label}</span>
     </div>
   );
