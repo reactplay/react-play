787f6d9589f60ea88af02954029418ba753c2743
Shivam-Katare <katare27451@gmail.com>
2022-10-29 08:56:10 +0530

    Updated the video music name

--- src/plays/2048/Game2048Components/VideoMusic.js
+++ src/plays/2048/Game2048Components/VideoMusic.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Vid from "../GameAudio/VideoMusic.mp4"
+import Video from "../GameAudio/VideoMusic.mp4"
 
-function VideoMusic() {
+function VideoMusic(props) {
   return (
     <>
       <div className='invisible'>
         <video width="20" height="40" controls autoPlay="autoplay" loop>
-          <source src={Vid} type="video/mp4" />
+          <source src={Video} type="video/mp4" />
         </video>
       </div>
     </>
