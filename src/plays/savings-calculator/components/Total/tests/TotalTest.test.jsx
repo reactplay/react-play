c3bdbce8885a85c86889706627dcf50ac8cef3db
Keerthivasan D <keerthi.vasan.d@gmail.com>
2022-10-30 12:07:31 +0530

    [Play]: Savings Calculator (#748)
    * Initial file setup with images
    
    * Add total component
    
    * Add saving options form
    
    * Add saving options form funtionality
    
    * Add total dynamic rendering functionality
    
    * Fix currency change automatically in form feilds
    
    * Refactor savings calculation
    
    * Add responsive styling
    
    * Add cover image
    
    * Add readme
    
    * Add play's header to play
    
    * Refactor SavingsCalculator to SavingCalculator
    
    * Refactor calculatSaving to getCalculatedSaving
    
    * Refactor currencies to select tag
    
    * Add play body wrapper
    
    * Add value attribute to dropdown options
    
    * Refactor currency symbol into util function
    
    * Refactor period options
    
    * Refactor wrap fireevent with act
    
    * Refactor appropriate variable type usage
    
    * Add prefix to parent classes to avoid conflicts
    
    * Add implementation details to readme
    
    * Refactor image import name
    
    Co-authored-by: Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>

---
+++ src/plays/savings-calculator/components/Total/tests/TotalTest.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Total from "../index";
+
+describe("Total: Basic rendering", () => {
+  test("should display total amount sent in props", () => {
+    const total = 2327.99;
+    const currency = "INR";
+
+    render(<Total total={total} currency={currency} />);
+
+    const totalElement = screen.getByText(/₹/);
+
+    expect(totalElement).toHaveTextContent(total);
+  });
+});
