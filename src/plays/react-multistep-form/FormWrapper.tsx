b2c3e8b1b00dac81f5e19a15516f54279dd607d6
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-11-12 10:17:12 +1100

    Create a form wrrapper

---
+++ src/plays/react-multistep-form/FormWrapper.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react";
+type FormWrapperProps = {
+  title: string;
+  children: ReactNode;
+};
+export function FormWrapper({ title, children }: FormWrapperProps) {
+  return (
+    <>
+      <h2 style={{ textAlign: "center", margin: 0, marginBottom: "2 rem" }}>
+        {title}
+      </h2>
+      <div
+        style={{
+          display: "grid",
+          gap: "1rem .5rem",
+          justifyContent: "flex-start",
+          gridTemplateColumns: "auto minmax(auto, 400px",
+        }}
+      >
+        {children}
+      </div>
+    </>
+  );
+}
