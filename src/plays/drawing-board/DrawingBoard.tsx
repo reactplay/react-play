bc9fde2710b7610a462be39d90aaf3d48acac669
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-11-18 20:53:04 +0530

    removed unneccessary comments

--- src/plays/drawing-board/DrawingBoard.tsx
+++ src/plays/drawing-board/DrawingBoard.tsx
@@ -13,7 +13,6 @@ export default function DrawingBoard(props: any) {
       <div className="play-details">
         <PlayHeader play={props} />
         <div className="play-details-body">
-          {/* Your Code Starts Here */}
           <React.StrictMode>
             <Provider store={store}>
               <CanvasProvider>
@@ -21,19 +20,8 @@ export default function DrawingBoard(props: any) {
               </CanvasProvider>
             </Provider>
           </React.StrictMode>
-          {/* Your Code Ends Here */}
         </div>
       </div>
     </>
   );
 }
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <CanvasProvider>
-        <App />
-      </CanvasProvider>
-    </Provider>
-  </React.StrictMode>
-);
