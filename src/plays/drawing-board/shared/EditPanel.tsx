e2e36dd37416787356de4cf49b3adeb98557c750
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-11-18 20:56:11 +0530

    merging import statements

--- src/plays/drawing-board/shared/EditPanel.tsx
+++ src/plays/drawing-board/shared/EditPanel.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { redo, undo } from "../modules/historyIndex/slice";
 import { strokesLengthSelector } from "../modules/strokes/slice";
 
