714ac0874dc63ac86106d2a7ae14ae57e9038d3f
Shyam Mahanta <63530626+Angryman18@users.noreply.github.com>
2022-11-23 21:19:44 +0530

    Issue-429-edit-play-flow (#532)
    * draft work of edit play
    
    * edit play on prod
    
    * edit play ongoing draft work
    
    * state reset fix
    
    * edit play ui
    
    * draft work
    
    * draft save
    
    * resolved error
    
    * updated edit play code
    
    * draft work
    
    * edit play draft work
    
    * fix broken create play flow
    
    Co-authored-by: Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
    Co-authored-by: koustov <koustov@live.com>

--- src/common/components/PlayForms.jsx
+++ src/common/components/PlayForms.jsx
@@ -1,91 +1,129 @@
-import {TextField , FormControl, Autocomplete} from '@mui/material';
-import { useEffect, useState } from 'react';
-import * as _ from 'lodash';
-const PlayForm = ({fields, data, onChange}) => {
+import { useEffect, useState, useRef } from "react";
+import { TextField, FormControl, Autocomplete, Button } from "@mui/material";
+import * as _ from "lodash";
 
-  const [formData, setFormData] = useState({})
+const disabledFields = ['issue', 'github']
+
+const PlayForm = ({ fields, formDataObj, onSubmit, isEditPlay}) => {
+
+  const [formData, setFormData] = useState({...formDataObj});
+  const formDataReference = useRef(formDataObj);
+
+  const checkDisabledInputs = (fieldName) => isEditPlay && disabledFields.includes(fieldName)
+
+
+  const handleChange = (key, value) => {
+    setFormData((pre) => ({...pre, [key]: value }));
+  };
+
   useEffect(() => {
-    setFormData({...data})
-  }, [data])
+    setFormData({ ...formDataReference.current });
+  }, []);
 
-  const onDataChanged = (key, value) => {
-    formData[key] = value;
-    setFormData({...formData})
-    if(onChange) {
-      onChange(formData)
+  const renderField = (field) => {
+    switch (field.type) {
+      case "input":
+        return (
+          <TextField
+            id={field.id}
+            label={field.plaeholder}
+            value={formData[field.datafield]}
+            size='small'
+            className='w-full'
+            disabled={checkDisabledInputs(field.datafield)}
+            {...field}
+            onChange={(e) => handleChange(field.datafield, e.target.value)}
+          />
+        );
+      case "select":
+        return (
+          <Autocomplete
+            id={field.datafield}
+            size='small'
+            options={field.options || []}
+            getOptionLabel={(option) => option.name || option[field.fieldName] || option}
+            filterSelectedOptions
+            multiple={field.multiple}
+            freeSolo={field.freeSolo}
+            disabled={checkDisabledInputs(field.datafield)}
+            value={formData[field.datafield] ?? ''}
+            onChange={(e, newValue) => {
+              let updatedval = newValue;
+              if (field.multiple) {
+                updatedval = [];
+                newValue.forEach((v) => {
+                  if (_.isObject(v)) {
+                    updatedval.push(v);
+                  } else {
+                    updatedval.push({
+                      [field.fieldName || "name"]: v,
+                      [field.fieldValue || "value"]: "",
+                    });
+                  }
+                });
+              }
+              handleChange(field.datafield, updatedval);
+            }}
+            renderInput={(params) => (
+              <TextField {...params} size='small' placeholder={field.placeholder} />
+            )}
+          />
+        );
+      default:
+        return null;
     }
-  }
+  };
 
-   const renderField = field => {
-    switch(field.type){
-      case 'input': return <TextField id={field.id} 
-                                      label={field.plaeholder}
-                                      value={formData[field.datafields]} 
-                                      size="small"
-                                      className='w-full'
-                                      {...field}
-
-                                      onChange={(e) => {
-                                        onDataChanged(field.datafield, e.target.value)}
-                                      }/>
-        case 'select': return <Autocomplete id={field.datafield}
-                                            size="small"
-                                            options={field.options || []}
-                                            getOptionLabel={(option) => option.name || option[field.fieldName] || option}
-                                            filterSelectedOptions
-                                            multiple={field.multiple}
-                                            freeSolo={field.freeSolo}
-                                            onChange={(e, newValue) => {
-                                              let updatedval = newValue;
-                                              if(field.multiple) {
-                                                updatedval = [];
-                                                newValue.forEach(v => {
-                                                  if(_.isObject(v)){
-                                                    updatedval.push(v);
-                                                  } else {
-                                                    updatedval.push({
-                                                      [field.fieldName || 'name']: v,
-                                                      [field.fieldValue || 'value']: ""
-                                                    })
-                                                  }
-                                                })
-                                              }
-                                              onDataChanged(field.datafield, updatedval)}
-                                            }
-                                            renderInput={(params) => (
-                                              <TextField
-                                                {...params}
-                                                size="small"
-                                                placeholder={field.placeholder}
-                                              />
-                                            )}
-                                />
-                default: return <></>
-    }
-  }
- 
   const renderForm = () => {
-    return <>
-        <FormControl className='w-full'>
-          {fields.map((field, field_key) => {
-            return(
-              <div className='flex p-2' key={field_key}>
-                <div className='flex-1'>
-                  {field.display}{field.required ? "*":""}
-                </div>
-                <div className='flex-1'>
-              {renderField(field)}
+    return (
+      <FormControl className='w-full'>
+        {fields.map((field, field_key) => {
+          return (
+            <div className='flex p-2' key={field_key}>
+              <div className='flex-1'>
+                {field.display}
+                {field.required ? "*" : ""}
               </div>
-              </div>
-              
-            )
-          })}
-        </FormControl> </>
-  }
-  
+              <div className='flex-1'>{renderField(field)}</div>
+            </div>
+          );
+        })}
+      </FormControl>
+    );
+  };
+
+  const isFieldsAreInValid = () => {
+    let res = false;
+    fields.forEach((tmpl) => {
+      if (tmpl.required && (!formData || !formData[tmpl.datafield])) {
+        if (isEditPlay && disabledFields.includes(tmpl.datafield)) {
+          res =  false
+        } else {
+          res = true;
+        }
+      }
+    });
+    return res;
+  };
+
   return (
     <>
-      {renderForm()}
+      <div className='flex-1 px-10 py-8 overflow-auto'>
+        <form>{renderForm()}</form>
+      </div>
+      <div className='h-14'>
+        <hr />
+        <div className='p-8 h-full flex items-center'>
+          <Button
+            size='small'
+            variant='contained'
+            disabled={isFieldsAreInValid()}
+            onClick={onSubmit.bind(null, formData)}
+          >
+            {!isEditPlay ? "Create the awesome" : "Edit Play"}
+          </Button>
+        </div>
+      </div>
     </>
   );
 };
