98d969cf7d1a75d2afbbcce40044c61ad553eeb1
Supriya M <30731236+supminn@users.noreply.github.com>
2022-12-02 10:15:36 +0530

    feat: search by text in filters (#778)
    * feat: search by text in filters
    
    * refactor: review changes; added common utils file
    
    * refactor: useEffect instead of useMemo
    
    Co-authored-by: Harshit Jain <hjain.tech@gmail.com>
    Co-authored-by: Shyam Mahanta <63530626+Angryman18@users.noreply.github.com>

---
+++ src/common/utils/commonUtils.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * @param {String} optionText
+ * @param {String} searchText
+ * @returns Boolean
+ */
+export const compareTextValue = (optionText, searchText) => {
+  if (typeof optionText === "string") {
+    return optionText.toLowerCase().indexOf(searchText.toLowerCase()) > -1;
+  }
+  return (
+    optionText?.props?.children[1]
+      .toLowerCase()
+      .indexOf(searchText.toLowerCase()) > -1
+  );
+};
+
+/**
+ *
+ * @param {String} path cover path
+ * @returns String
+ */
+export const getProdUrl = (path) => {
+  return `https://reactplay.io${path}`;
+};
+
+/**
+ *
+ * @param {Array} obj
+ * @param {String} key
+ * @param {String} value
+ * @returns Number
+ */
+export default function countByProp(obj, key, value) {
+  return obj.reduce((acc, item) => (item?.[key] === value ? ++acc : acc), 0);
+}
