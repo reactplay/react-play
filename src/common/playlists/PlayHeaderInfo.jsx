2f68e9f1f2bc49462f1dd92e80e18717680c089e
Ankit Kumar <myselfankit51@gmail.com>
2023-01-11 00:48:37 +0530

    added date with locale

--- src/common/playlists/PlayHeaderInfo.jsx
+++ src/common/playlists/PlayHeaderInfo.jsx
@@ -1,19 +1,33 @@
 import { IoMdArrowBack } from 'react-icons/io';
 import { Link } from 'react-router-dom';
 import LevelBadge from 'common/components/LevelBadge';
+import { format } from 'date-fns';
+import * as allLocales from 'date-fns/locale';
 
-const Author = ({ user, githubUsername }) => {
+const Author = ({ user, githubUsername, playCreatedAt }) => {
+  function formatPlayDate() {
+    const locale = navigator.language.split('-').join('');
+    const dnsLocale = allLocales[locale] ?? allLocales.enUS;
+
+    return format(new Date(playCreatedAt), 'dd MMMM, yyyy', {
+      locale: dnsLocale
+    });
+  }
+
   return (
-    <div className="header-author flex items-center gap-2">
+    <div className="flex items-center gap-2 header-author">
       <img alt="avatar" className="rounded-full" height="25px" src={user?.avatarUrl} width="25px" />
-      <a
-        className="play-anchor"
-        href={`https://github.com/${githubUsername}`}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <strong>{user?.displayName}</strong>
-      </a>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <a
+          className="play-anchor"
+          href={`https://github.com/${githubUsername}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <strong>{user?.displayName}</strong>
+        </a>
+        <small className="header-desc">Created on {formatPlayDate()}</small>
+      </div>
     </div>
   );
 };
@@ -32,7 +46,7 @@ const Tags = ({ tags }) => {
 
 const PlayHeaderInfo = ({ play }) => {
   return (
-    <div className="header-leftcol overflow-hidden">
+    <div className="overflow-hidden header-leftcol">
       <div className="header-leftcol-action">
         <Link className="action" to="/plays">
           <IoMdArrowBack className="icon" size="24px" />
@@ -47,8 +61,10 @@ const PlayHeaderInfo = ({ play }) => {
             {!!play.play_tags.length && <Tags tags={play.play_tags} />}
           </div>
         </div>
-        <div className="header-secondary">
-          {play.user && <Author githubUsername={play.github} user={play.user} />}
+        <div className="mt-1 header-secondary">
+          {play.user && (
+            <Author githubUsername={play.github} user={play.user} playCreatedAt={play.created_at} />
+          )}
         </div>
       </div>
     </div>
