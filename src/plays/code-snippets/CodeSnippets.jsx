513ffba9c295ab4b06cb6bc6eef8398a4706a973
senali <senalidilumika@gmail.com>
2023-01-12 22:26:48 +0530

    FIx linting issues

--- src/plays/code-snippets/CodeSnippets.jsx
+++ src/plays/code-snippets/CodeSnippets.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import PlayHeader from "common/playlists/PlayHeader";
-import MEditor from "./components/editor";
-import { ScreenCapture } from "react-screen-capture";
+import React, { useState } from 'react';
+import PlayHeader from 'common/playlists/PlayHeader';
+import MEditor from './components/editor';
+import { ScreenCapture } from 'react-screen-capture';
 
 function CodeSnippets(props) {
   const [screenCapture, setScreenCapture] = useState();
-  const [snippets, setSnippets] = useState("");
-  
+  const [snippets, setSnippets] = useState('');
+
   const handleScreenCapture = (screenCapture) => {
     setScreenCapture(screenCapture);
   };
 
   const handleSave = () => {
     const screenCaptureSource = screenCapture;
-    const downloadLink = document.createElement("a");
-    const fileName = "react-screen-capture.png";
+    const downloadLink = document.createElement('a');
+    const fileName = 'react-screen-capture.png';
 
     downloadLink.href = screenCaptureSource;
     downloadLink.download = fileName;
@@ -43,19 +43,22 @@ function CodeSnippets(props) {
                   Capture Code
                 </button>
                 <button
-                className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => setScreenCapture()}
-              >
-                Clear
-              </button>
+                  className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={() => setScreenCapture()}
+                >
+                  Clear
+                </button>
               </div>
               <div>
-                {screenCapture && (
-                  <img src={screenCapture} alt="react-screen-capture" />
-                )}
+                {screenCapture && <img alt="react-screen-capture" src={screenCapture} />}
                 <p>
                   {screenCapture && (
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3" onClick={handleSave}>Download</button>
+                    <button
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3"
+                      onClick={handleSave}
+                    >
+                      Download
+                    </button>
                   )}
                 </p>
               </div>
--- src/plays/code-snippets/components/editor.jsx
+++ src/plays/code-snippets/components/editor.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 import Editor from '@monaco-editor/react';
 
-const MEditor = ({onChange, data}) => {
+const MEditor = ({ onChange, data }) => {
   return (
     <Editor
-     height="60vh"
-     defaultLanguage="javascript"
-     defaultValue="// some comment"
-     onChange={onChange}
-     value={data}
-     theme="vs-dark"
-   />
-  )
-}
+      defaultLanguage="javascript"
+      defaultValue="// some comment"
+      height="60vh"
+      theme="vs-dark"
+      value={data}
+      onChange={onChange}
+    />
+  );
+};
 
-export default MEditor
+export default MEditor;
