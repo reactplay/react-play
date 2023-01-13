85adf60c6007a2505d6b9a43876fa5725aefba9c
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-01-14 08:30:57 +1100

    Revert changes done in others repository

--- src/plays/react-multistep-form/FormWrapper.tsx
+++ src/plays/react-multistep-form/FormWrapper.tsx
@@ -1,13 +1,14 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from 'react';
 type FormWrapperProps = {
   title: string;
   children: ReactNode;
 };
+
 export function FormWrapper({ title, children }: FormWrapperProps) {
   return (
     <>
-      <h2 className='form-wrapper-heading'>{title}</h2>
-      <div className='form-wrapper-content-grid'>{children}</div>
+      <h2 className="form-wrapper-heading">{title}</h2>
+      <div className="form-wrapper-content-grid">{children}</div>
     </>
   );
 }
