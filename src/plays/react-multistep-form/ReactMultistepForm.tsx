daf4739de03414fe3c2dfb8c1f34abcc04668caf
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-01-16 11:39:43 +1100

    Include Header to play

--- src/plays/react-multistep-form/ReactMultistepForm.tsx
+++ src/plays/react-multistep-form/ReactMultistepForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PlayHeader from 'common/playlists/PlayHeader';
 import { useMultistepForm } from './customHook/useMultistepForm';
 import PersonalInfo from './personalInfo';
 import ContactInfo from './contactInfo';
@@ -20,7 +21,7 @@ const INITIAL_DATA: FormData = {
   password: ''
 };
 
-function ReactMultistepForm() {
+function ReactMultistepForm(props: any) {
   const [data, setData] = useState(INITIAL_DATA);
   const [showReview, setShowReview] = useState(false);
 
@@ -45,52 +46,57 @@ function ReactMultistepForm() {
 
   return (
     <>
-      {!showReview && (
-        <div className="container multistep-container">
-          <form onSubmit={handleSubmit}>
-            <div className="steps">
-              {currentStepIndex + 1}/{totalSteps}
+      <div className="play-details">
+        <PlayHeader play={props} />
+        <div className="play-details-body ">
+          {!showReview && (
+            <div className="container multistep-container">
+              <form onSubmit={handleSubmit}>
+                <div className="steps">
+                  {currentStepIndex + 1}/{totalSteps}
+                </div>
+                {step}
+                <div className="form-steps">
+                  {!isFirstStep && (
+                    <button className="multistep-button" type="button" onClick={back}>
+                      Back
+                    </button>
+                  )}
+
+                  <button className="multistep-button" type="submit">
+                    {isLastStep ? 'Finish' : 'Next'}
+                  </button>
+                </div>
+              </form>
             </div>
-            {step}
-            <div className="form-steps">
-              {!isFirstStep && (
-                <button className="multistep-button" type="button" onClick={back}>
+          )}
+          {showReview && (
+            <div className="container multistep-container">
+              <div>
+                <h5>Thank you for submitting. Please review your details.</h5>
+                <p>First Name: {data.firstName}</p>
+                <p>Last Name: {data.lastName}</p>
+                <p>Address: {data.address}</p>
+                <p>Email: {data.email}</p>
+              </div>
+              <div className="form-steps">
+                <button
+                  className="multistep-button"
+                  type="button"
+                  onClick={() => {
+                    setShowReview(false);
+                  }}
+                >
                   Back
                 </button>
-              )}
-
-              <button className="multistep-button" type="submit">
-                {isLastStep ? 'Finish' : 'Next'}
-              </button>
+              </div>
+              <div>
+                <p>Please press Back button to edit your details.</p>
+              </div>
             </div>
-          </form>
+          )}
         </div>
-      )}
-      {showReview && (
-        <div className="container multistep-container">
-          <div>
-            <h5>Thank you for submitting. Please review your details.</h5>
-            <p>First Name: {data.firstName}</p>
-            <p>Last Name: {data.lastName}</p>
-            <p>Address: {data.address}</p>
-            <p>Email: {data.email}</p>
-          </div>
-          <div className="form-steps">
-            <button
-              className="multistep-button"
-              type="button"
-              onClick={() => {
-                setShowReview(false);
-              }}
-            >
-              Back
-            </button>
-          </div>
-          <div>
-            <p>Please press Back button to edit your details.</p>
-          </div>
-        </div>
-      )}
+      </div>
     </>
   );
 }
