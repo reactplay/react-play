a2b7bc62969b9202ba3db0cb187ff75c2a9b2cb4
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-01-20 20:13:02 +1100

    Implement multistep form (#804)
    * Update Readme as mistakely delete it
    
    * Create custom hoook for form handling
    
    * Create a form wrrapper
    
    * Create JSX CSS ffor form steps
    
    * Implementation LLogic for multistep form
    
    * Conver .jsx ext to .tsx for index file
    
    * Minor Fixes JSX aand CSS
    
    * Add cover image for play
    
    * Remove unused csss
    
    * Remove unused connsole logs
    
    * Typo fixes / Css fixes
    
    * Update Readme.md
    
    * Remove inline CSSS, Follow BEM convention
    
    * Final changes before pull request
    
    * Fix the formatting and linting errors
    
    * Revert changes done in others repository
    
    * Change the override selector. Add an unique class name
    
    * Show output in a Styled Div instead of alert
    
    * Include Header to play
    
    * Design Fix: Add padding and margin on small screen
    
    * Prefix CSS classes and Refactor Code
    
    Co-authored-by: Koustov <koustov@live.com>
    Co-authored-by: Tapas Adhikary <tapas.adhikary@gmail.com>

---
+++ src/plays/react-multistep-form/customHook/useMultistepForm.ts
@@ -0,0 +1,37 @@
+import { ReactElement } from 'react';
+import { useState } from 'react';
+
+export function useMultistepForm(steps: ReactElement[]) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  function next() {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i;
+
+      return i + 1;
+    });
+  }
+
+  function back() {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i;
+
+      return i - 1;
+    });
+  }
+
+  function goTo(index: number) {
+    setCurrentStepIndex(index);
+  }
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    back,
+    next,
+    goTo,
+    totalSteps: steps.length,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1
+  };
+}
