4f6d02977c74bea99ef3fffc753fe569c9619d81
Nirmal Kumar <nirmalkc@gmail.com>
2023-02-08 12:09:51 +0530

    Fixing banner promo issue

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -102,7 +102,7 @@ const Header = () => {
   return (
     <>
       <header
-        className={`app-header ${showHideBits.setHeaderStyle ? '' : ' app-header-home'}`}
+        className={`app-header ${showHideBits.setHeaderStyle ? '' : ' app-header-home app-header-home--promo '}`}
         data-testid="app-header"
       >
         <span>
