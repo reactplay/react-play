8c241ff66d77cb8b9f224e100ce8e6503c204133
Ankit Kanyal <35369589+ANKIT9761@users.noreply.github.com>
2023-02-17 23:39:16 +0530

    [#2PlaysAMonth]:Github UserFinder Completed (#943)
    * Fixes-Bug#903: Fixed Server break due to linting issues
    
    * [#2PlaysAMonth]:Github UserFinder Completed
    
    * [#2PlaysAMonth]:Done with asked changes
    
    * Made changes as requested
    
    * Made Styles Changes so that it doesn't overlap and override other styles.
    
    * Done with requested changes and edited play and uploaded the stackstream video.
    
    * Fix:Fixed Linting issues
    
    ---------
    
    Co-authored-by: Tapas Adhikary <tapas.adhikary@gmail.com>
    Co-authored-by: Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>

--- src/common/utils/commonUtils.js
+++ src/common/utils/commonUtils.js
@@ -31,3 +31,14 @@ export const getProdUrl = (path) => {
 export default function countByProp(obj, key, value) {
   return obj.reduce((acc, item) => (item?.[key] === value ? ++acc : acc), 0);
 }
+
+export function formatDate(data) {
+  if (data === undefined) return '';
+  const dataarr = data.split('T')[0].split('-');
+  const day = dataarr[2];
+  const month = dataarr[1];
+  const year = dataarr[0];
+  const datemonth = Date(year, month, day).split(' ');
+
+  return `Joined ${day} ${datemonth[1]} ${year}`;
+}
