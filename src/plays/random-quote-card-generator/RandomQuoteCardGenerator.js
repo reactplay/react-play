c5860aa8b6dafa55f610f3bafa9159a4b5441c64
siddhantsiddh15 <siddhant.bitm020@gmail.com>
2023-02-21 20:37:06 +0530

    Random Quote Generator #973 was causing conflict. Resolved it.

--- src/plays/random-quote-card-generator/RandomQuoteCardGenerator.js
+++ src/plays/random-quote-card-generator/RandomQuoteCardGenerator.js
@@ -3,21 +3,6 @@ import SideBarDesign from './Components/SideBarDesign';
 import CardContainer from './Components/CardContainer';
 import { useEffect, useState } from 'react';
 import SideBarOperation from './Components/SideBarOperation';
-
-
-const gradients = [
-  { id: '1', gradient_color1: '#FF9966', gradient_color2: '#FF5E62' },
-  { id: '2', gradient_color1: '#EC008C', gradient_color2: '#FC6767' },
-  { id: '3', gradient_color1: '#4646FB', gradient_color2: '#BE4DFF' },
-  { id: '4', gradient_color1: '#2D47A2', gradient_color2: '#00FFFF' },
-  { id: '5', gradient_color1: '#870FFF', gradient_color2: '#F89B29' },
-  { id: '6', gradient_color1: '#B1FE64', gradient_color2: '#24C75B' },
-  { id: '7', gradient_color1: '#BAFFAD', gradient_color2: '#ADCEFF' },
-  { id: '8', gradient_color1: '#FFC7CA', gradient_color2: '#E299FF' }
-];
-
-function RandomQuoteCardGenerator(props) {
-
 import { gradients } from './data';
 
 function RandomQuoteCardGenerator(props) {
@@ -28,22 +13,12 @@ function RandomQuoteCardGenerator(props) {
     }
   ];
 
-
   const [gradientColor, setGradientColor] = useState([gradients[0]]);
   const [quote, setQuote] = useState({});
   const [regenerate, setRegenerate] = useState(false);
   const [AspectRatio, setAspectRatio] = useState('Square');
   const [cardColor, setCardColor] = useState('Light');
   const [domImg, setDomImg] = useState({});
-
-
-  const apiUrl = 'https://api.quotable.io/random';
-  useEffect(() => {
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((data) => setQuote(data))
-      .catch((err) => console.error(err));
-
   const [backupData, setBackupData] = useState(initialQuoteData);
   const [apiStatus, setApiStatus] = useState(true);
 
@@ -66,7 +41,6 @@ function RandomQuoteCardGenerator(props) {
     };
 
     load();
-
   }, [regenerate]);
 
   return (
@@ -84,11 +58,8 @@ function RandomQuoteCardGenerator(props) {
             />
             <CardContainer
               AspectRatio={AspectRatio}
-
-
               apiStatus={apiStatus}
               backupData={backupData}
-
               cardColor={cardColor}
               domImg={domImg}
               gradientColor={gradientColor}
@@ -99,20 +70,14 @@ function RandomQuoteCardGenerator(props) {
             />
             <SideBarOperation
               AspectRatio={AspectRatio}
-
-
               apiStatus={apiStatus}
-
               cardColor={cardColor}
               domImg={domImg}
               gradientColor={gradientColor}
               gradients={gradients}
               regenerate={regenerate}
               setAspectRatio={setAspectRatio}
-
-
               setBackupData={setBackupData}
-
               setCardColor={setCardColor}
               setGradientColor={setGradientColor}
               setRegenerate={setRegenerate}
