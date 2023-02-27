2a908aa3ae1424e9c005f6bdbc47355aa6b47b5c
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-02-27 21:57:07 +0530

    marker fix

--- src/plays/travel-advisor/components/Map.jsx
+++ src/plays/travel-advisor/components/Map.jsx
@@ -2,6 +2,16 @@ import React, { useEffect } from 'react';
 import { MapContainer, Popup, TileLayer, useMap, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import '../styles.css';
+
+import L from 'leaflet';
+
+const customMarker = new L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png',
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40]
+});
+
 const Map = ({ latitude, longitude, resdata, setlatitude, setlongitude }) => {
   const GetCoordinates = () => {
     const map = useMap();
@@ -39,7 +49,7 @@ const Map = ({ latitude, longitude, resdata, setlatitude, setlongitude }) => {
             ];
 
             return (
-              <Marker key={i} position={position}>
+              <Marker icon={customMarker} key={i} position={position}>
                 <Popup>
                   {item.name}
                   <img src={item.photo ? item.photo.images.medium.url : ''} />
