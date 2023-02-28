b8ff7ff1f2e4dbae3af8ca01d5b9c033afd0c93e
raibove <shwetakale144@gmail.com>
2023-02-28 16:54:35 +0530

    Add. color picker

--- src/plays/pixy/components/drawing-panel/DrawingPanel.jsx
+++ src/plays/pixy/components/drawing-panel/DrawingPanel.jsx
@@ -1,11 +1,16 @@
 import './DrawingPanel.css';
 import { useState } from 'react';
+import {CirclePicker} from "react-color";
 import Row from '../row/Row';
 
 function DrawingPanel() {
   const [panelWidth, setPanelWidth] = useState(16);
   const [panelHeight, setPanelHeight] = useState(16);
-  const [selectedColor, setColor] = useState('#f44336');
+  const [selectedColor, setColor] = useState('#4caf50');
+
+  function changeColor(color) {
+    setColor(color.hex);
+  }
 
   function getRows() {
     let rows = [];
@@ -20,6 +25,7 @@ function DrawingPanel() {
   return (
     <div className="drawingPanel">
       <h2>Create Pixel Art</h2>
+      <CirclePicker color={selectedColor} onChangeComplete={changeColor} />
       <div>{getRows()}</div>
     </div>
   );
