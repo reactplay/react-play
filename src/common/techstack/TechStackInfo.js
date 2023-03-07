1e6baef22953da63eb4a13e035217391a52969e9
Jerom Palimattom Tom <83979298+jerom-tom@users.noreply.github.com>
2023-03-07 22:48:02 +0530

    Adding Categorisation

--- src/common/techstack/TechStackInfo.js
+++ src/common/techstack/TechStackInfo.js
@@ -22,96 +22,112 @@ export const TechStackInfo = [
     comp: SiReact,
     text: 'ReactJS',
     type: 'icon',
-    link: 'https://reactjs.org'
+    link: 'https://reactjs.org',
+    category: 'Frontend'
   },
   {
     comp: SiRedux,
     text: 'Redux',
     type: 'icon',
-    link: 'https://redux.js.org'
+    link: 'https://redux.js.org',
+    category: 'Frontend'
   },
   {
     comp: NHost,
     text: 'NHost',
     type: 'image',
-    link: 'https://nhost.io'
+    link: 'https://nhost.io',
+    category: 'Backend'
   },
   {
     comp: SiGit,
     text: 'Git',
     type: 'icon',
-    link: 'https://git-scm.com'
+    link: 'https://git-scm.com',
+    category: 'DevOps'
   },
   // {
   //   comp: SiMaterialui,
   //   text: 'Material UI',
   //   type: 'icon',
-  //   link: 'https://mui.com'
+  //   link: 'https://mui.com',
+  //   category: 'Frontend'
   // },
   {
     comp: SiTailwindcss,
     text: 'Tailwind',
     type: 'icon',
-    link: 'https://tailwindcss.com'
+    link: 'https://tailwindcss.com',
+    category: 'Frontend'
   },
   {
     comp: SiHasura,
     text: 'Hasura',
     type: 'icon',
-    link: 'https://hasura.io'
+    link: 'https://hasura.io',
+    category: 'Backend'
   },
   {
     comp: SiPostgresql,
     text: 'PostgreSQL',
     type: 'icon',
-    link: 'https://www.postgresql.org'
+    link: 'https://www.postgresql.org',
+    category: 'Backend'
   },
   {
     comp: SiJavascript,
     text: 'Javascript',
     type: 'icon',
-    link: 'https://www.ecma-international.org/'
+    link: 'https://www.ecma-international.org/',
+    category: 'Frontend'
   },
   {
     comp: SiFirebase,
     text: 'Firebase',
     type: 'icon',
-    link: 'https://firebase.google.com'
+    link: 'https://firebase.google.com',
+    category: 'Backend'
   },
   {
     comp: SiTypescript,
     text: 'Typescript',
     type: 'icon',
-    link: 'https://www.typescriptlang.org'
+    link: 'https://www.typescriptlang.org',
+    category: 'Frontend'
   },
   {
     comp: SiGraphql,
     text: 'Graphql',
     type: 'icon',
-    link: 'https://graphql.org'
+    link: 'https://graphql.org',
+    category: 'Backend'
   },
   {
     comp: SiDocker,
     text: 'Docker',
     type: 'icon',
-    link: 'https://www.docker.com'
+    link: 'https://www.docker.com',
+    category: 'Backend'
   },
   {
     comp: SiNodedotjs,
     text: 'NodeJS',
     type: 'icon',
-    link: 'https://nodejs.org/en'
+    link: 'https://nodejs.org/en',
+    category: 'Backend'
   },
   {
     comp: SiPython,
     text: 'Python',
     type: 'icon',
-    link: 'https://www.python.org'
+    link: 'https://www.python.org',
+    category: 'Backend'
   },
   {
     comp: Umami,
     text: 'Umami',
     type: 'image',
-    link: 'https://umami.is/'
+    link: 'https://umami.is/',
+    category: 'Analytics'
   }
 ];
--- src/common/techstack/TechStacks.jsx
+++ src/common/techstack/TechStacks.jsx
@@ -1,43 +1,41 @@
 import { TechStackInfo } from './TechStackInfo';
 
+const TechStackCategory = ({ category }) => {
+  const filteredTechStack = TechStackInfo.filter((item) => item.category === category);
+
+  return (
+    <div>
+      <h2>{category}</h2>
+      <ul>
+        {filteredTechStack.map((item, idx) => (
+          <li key={idx}>
+            {item.type === 'icon' ? (
+              <a href={item.link} target="_blank" rel="noreferrer">
+                <item.comp size="24" />
+                <span>{item.text}</span>
+              </a>
+            ) : (
+              <a href={item.link} target="_blank" rel="noreferrer">
+                <img src={item.comp} alt={item.text} />
+                <span>{item.text}</span>
+              </a>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const TechStack = () => {
   return (
     <main className="app-body">
       <h1 className="section-title text-center pt-8 mt-48">ReactPlay is proudly powered by</h1>
       <div className="list-brand-tech-stack">
-        {/* <div className='md:w-[800px] 2xl:w-[1000px] w-full flex flex-wrap m-8'> */}
-        {TechStackInfo.map((Item, idx) => {
-          if (Item.type === 'icon') {
-            return (
-              <a
-                className="brand-tech-stack"
-                href={Item.link}
-                key={idx}
-                rel="noreferrer"
-                target="_blank"
-              >
-                {/* eslint-disable react/jsx-pascal-case */}
-                <Item.comp className="icon" size="80" />
-                <p>{Item.text}</p>
-              </a>
-            );
-          } else if (Item.type === 'image') {
-            return (
-              <a
-                className="brand-tech-stack"
-                href={Item.link}
-                key={idx}
-                rel="noreferrer"
-                target="_blank"
-              >
-                <img alt={Item.comp} className="image" src={Item.comp} />
-                <p className="text-center">{Item.text}</p>
-              </a>
-            );
-          } else {
-            return null;
-          }
-        })}
+        <TechStackCategory category="Frontend" />
+        <TechStackCategory category="Backend" />
+        <TechStackCategory category="DevOps" />
+        <TechStackCategory category="Analytics" />
       </div>
     </main>
   );
