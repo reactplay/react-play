6c789b58414e168d34cdd25dc9245553f4e98dd5
WhyNotSantosh <san_sriramadasu@yahoo.com>
2023-04-01 00:20:26 +0530

    updated tube2tunes naming

--- src/plays/tube2tunes/Tube2tunes.js
+++ src/plays/tube2tunes/Tube2tunes.js
@@ -7,7 +7,7 @@ import Loader from './components/Loader';
 import TubeTunesLogo from './components/tubeTuneLogo.svg';
 
 // WARNING: Do not change the entry componenet name
-function Tube2tunes(props) {
+function Tube2Tunes(props) {
   // Your Code Start below.
 
   const inputUrlRef = useRef();
@@ -154,4 +154,4 @@ function Tube2tunes(props) {
   );
 }
 
-export default Tube2tunes;
+export default Tube2Tunes;
