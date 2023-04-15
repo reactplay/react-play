77b6adffce232f5fb02ec26c2e1ba7e23a4a0e55
Suzy <sulagnaghosh52@gmail.com>
2023-04-15 16:38:23 +0530

    feat: add all contributors

--- src/common/home/AllContributors.jsx
+++ src/common/home/AllContributors.jsx
@@ -5,7 +5,39 @@ function AllContributors() {
 
   return (
     <div>
-      <h1>All Contributors</h1>
+      <h2 className="title-primary">
+        <strong>
+          <span>Big Thanks</span>
+        </strong>
+        <br /> to All Contributors!
+      </h2>
+      <ul className="list-contributors">
+        {isLoading && <li>Loading...</li>}
+        {error && <li>Error: {error.message}</li>}
+        {data &&
+          data.map((contributor) => (
+            <li
+              className="contributor"
+              data-testid={`contributor-${contributor.id}`}
+              key={contributor.id}
+            >
+              <a
+                className="contributor-anchor"
+                href={contributor.html_url}
+                rel="noopener noreferrer"
+                target="_blank"
+                title={`${contributor.login}(${contributor.contributions} contributions)`}
+              >
+                <img
+                  alt={contributor.login}
+                  className="contributor-thumb"
+                  loading="lazy"
+                  src={contributor.avatar_url}
+                />
+              </a>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
