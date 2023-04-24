eabaf1beccf647b557fd4c23e4386e43eed1d710
npiyush97 <npiyush35@gmail.com>
2023-04-24 14:34:53 +0530

    fix #1064 #1075 and conflit with #774

--- src/common/playlists/DynamicBanner.jsx
+++ src/common/playlists/DynamicBanner.jsx
@@ -3,27 +3,22 @@ import thumbPlay from 'images/thumb-play.png';
 import { Link } from 'react-router-dom';
 import { MdArrowRightAlt } from 'react-icons/md';
 import { loadCoverImage } from 'common/utils/coverImageUtil';
+import Loader from 'common/spinner/spinner';
 
 const DynamicBanner = ({ randomPlay }) => {
   const [coverImage, setCoverImage] = useState(null);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
-
     async function setPlayCover() {
-      if (loading && randomPlay && randomPlay.cover) {
+      if (randomPlay && randomPlay.cover) {
         setCoverImage(randomPlay.cover);
-        setLoading(false);
       } else {
-        setLoading(true);
         // if it is not passed as a meta data
         // check in the play folder for a cover image with the name cover
         const coverImage = await loadCoverImage(randomPlay.slug);
 
         if (coverImage) {
           setCoverImage(coverImage);
-          setLoading(false);
         } else {
           setCoverImage(thumbPlay);
 
@@ -36,30 +31,33 @@ const DynamicBanner = ({ randomPlay }) => {
 
     setPlayCover();
   }, [randomPlay]);
-
-  if (loading) return <p>loading...</p>;
+  // check whether coverimage is assigned if not show loader.
 
   return (
     <Fragment>
-      <div
-        className="dynamic-banner-container"
-        style={{
-          background: `linear-gradient(rgba(0,0,0,0.5), #020808),url(${coverImage} ) center/cover no-repeat`,
-          minHeight: '50vh'
-        }}
-      >
-        <div className="dynamic-banner-body md:pl-14 px-4 py-2 md:py-3">
-          <h1 className="text-white text-3xl md:text-4xl lg:text-5xl">{randomPlay.name}</h1>
-          <p className="text-gray-400 mt-2 text-xs md:text-base ">{randomPlay.description}</p>
-          <Link to={`/plays/${encodeURI(randomPlay?.github?.toLowerCase())}/${randomPlay.slug}`}>
-            <button className="banner-button rounded-full font-extrabold uppercase px-8 md:px-12 md:py-1">
-              Let's Play <MdArrowRightAlt className="right-arrow-icon" size={40} />
-            </button>
-          </Link>
+      {coverImage === null ? (
+        <Loader />
+      ) : (
+        <div
+          className="dynamic-banner-container"
+          style={{
+            background: `linear-gradient(rgba(0,0,0,0.5), #020808),url(${coverImage} ) center/cover no-repeat`,
+            minHeight: '50vh'
+          }}
+        >
+          <div className="dynamic-banner-body md:pl-14 px-4 py-2 md:py-3">
+            <h1 className="text-white text-3xl md:text-4xl lg:text-5xl">{randomPlay.name}</h1>
+            <p className="text-gray-400 mt-2 text-xs md:text-base ">{randomPlay.description}</p>
+            <Link to={`/plays/${encodeURI(randomPlay?.github?.toLowerCase())}/${randomPlay.slug}`}>
+              <button className="banner-button rounded-full font-extrabold uppercase px-8 md:px-12 md:py-1">
+                Let's Play <MdArrowRightAlt className="right-arrow-icon" size={40} />
+              </button>
+            </Link>
 
-          {/* <Link to={``}><button className='banner-button'>See Creator's profile</button></Link> */}
+            {/* <Link to={``}><button className='banner-button'>See Creator's profile</button></Link> */}
+          </div>
         </div>
-      </div>
+      )}
     </Fragment>
   );
 };
