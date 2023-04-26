340dce4952561b5941e2400107339e2175da6eef
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-04-26 22:03:47 +0530

    fix: build linting

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,1 @@
-export const CREATE_PLAY_DOC_LINK = "https://docs.reactplay.io/How-To-Guides/how-to-create-play";
-
+export const CREATE_PLAY_DOC_LINK = 'https://docs.reactplay.io/How-To-Guides/how-to-create-play';
