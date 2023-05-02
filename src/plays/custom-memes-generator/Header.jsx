abbfb78f6eaa36073979dacb93c4f62eebe050c4
Amanaakash <amanakashofficial@gmail.com>
2023-05-02 22:48:49 +0545

    className for header image logo changed to cmg-header-troll-logo from troll-logo

--- src/plays/custom-memes-generator/Header.jsx
+++ src/plays/custom-memes-generator/Header.jsx
@@ -3,7 +3,7 @@ import logo from './troll-face.png';
 export default function Header() {
   return (
     <div className="cmg-header">
-      <img alt="logo" className="troll-logo" src={logo} />
+      <img alt="logo" className="cmg-header-troll-logo" src={logo} />
       <p className="cmg-header-title">
         <b>Meme Generator</b>
       </p>
