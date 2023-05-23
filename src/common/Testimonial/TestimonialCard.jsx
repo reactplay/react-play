1bc8c9dcd4a9487b24b0d38d52b68f09fc6ccc41
yung-coder <chandel.sanjeev5@gmail.com>
2023-05-24 02:22:12 +0530

    first-push

--- src/common/Testimonial/TestimonialCard.jsx
+++ src/common/Testimonial/TestimonialCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { format } from 'date-fns';
 import * as allLocales from 'date-fns/locale';
+import './Testimonial.css'
 
 const TestimonialCard = ({ quote, title, name, avatarUrl, codeName, created_at }) => {
   const [formattedDate] = useState(() => {
@@ -11,13 +12,13 @@ const TestimonialCard = ({ quote, title, name, avatarUrl, codeName, created_at }
     });
   });
   return (
-    <div className="flex h-[500px] justify-center items-center">
+    <div className="flex h-[700px] md:h-[500px] justify-center items-center">
       <div className="relative mx-auto  flex  w-[300px] md:w-[500px]  flex-col space-y-3 rounded-xl border border-gray-300 bg-white p-2  shadow-lg ">
         <div className="flex flex-col">
           <div className="flex justify-start items-start ">
             <div className="flex w-[100%] flex-col justify-start items-start space-y-4 bg-white  p-6">
               <h3 className="text-xl font-black text-gray-800 md:text-3xl">{title}</h3>
-              <p className="text-base text-left text-gray-500 overflow-scroll max-h-40">{quote}</p>
+              <p className="bar text-base text-left text-gray-500 overflow-y-scroll overflow-x-hidden max-h-40">{quote}</p>
             </div>
           </div>
           <div className="flex flex-col md:flex-row md:space-x-24  items-center  p-5">
