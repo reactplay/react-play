037f494489d663d387513a530d0d803d34f53d18
Ammaar Aslam <ammaaraslam7@gmail.com>
2023-06-10 17:03:08 +0530

    created structure & styling for event sponsors/partners

--- src/common/home/Sponsors.jsx
+++ src/common/home/Sponsors.jsx
@@ -1,4 +1,36 @@
+import { FaHandshake } from 'react-icons/fa';
+
 const Sponsors = () => {
+  const eventSponsors = [
+    {
+      name: '',
+      website: '',
+      logo: ''
+    },
+    {
+      name: '',
+      website: '',
+      logo: ''
+    }
+  ];
+  const eventPartners = [
+    {
+      name: '',
+      website: '',
+      logo: ''
+    },
+    {
+      name: '',
+      website: '',
+      logo: ''
+    },
+    {
+      name: '',
+      website: '',
+      logo: ''
+    }
+  ];
+
   return (
     <>
       <h2 className="title-primary">
@@ -6,6 +38,34 @@ const Sponsors = () => {
           <span>Our Partners & Sponsors</span>
         </strong>
       </h2>
+      <div className="event-partners-sponsors-container">
+        <div className="event-partners-sponsors">
+          <h3>Event Partners</h3>
+          <div className="content">
+            <div className="partners-sponsors-grid">
+              {eventPartners.map((e) => (
+                <a className="partners-sponsors-grid-item">abcd</a>
+              ))}
+            </div>
+            <button className="partners-sponsors-cta">
+              Partner an Event <FaHandshake className="handshake" size={28} />
+            </button>
+          </div>
+        </div>
+        <div className="event-partners-sponsors">
+          <h3>Event Sponsors</h3>
+          <div className="content">
+            <div className="partners-sponsors-grid">
+              {eventSponsors.map((e) => (
+                <a className="partners-sponsors-grid-item">abcd</a>
+              ))}
+            </div>
+            <button className="partners-sponsors-cta">
+              Sponsor an Event <FaHandshake className="handshake" size={28} />
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
