5299a69d78751f845a4639a76775557c5c5da94c
Ammaar Aslam <ammaaraslam7@gmail.com>
2023-06-10 14:46:15 +0530

    Initial commit for home sponsors section

---
+++ src/common/home/Sponsors.jsx
@@ -0,0 +1,13 @@
+const Sponsors = () => {
+  return (
+    <>
+      <h2 className="title-primary">
+        <strong>
+          <span>Our Partners & Sponsors</span>
+        </strong>
+      </h2>
+    </>
+  );
+};
+
+export default Sponsors;
