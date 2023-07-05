30ec1f7ebbbce33dee6e214fe7fa82bcb8579c0e
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-07-05 19:10:30 +0530

    More styling changes

--- src/plays/react-transitions/ReactTransitions.js
+++ src/plays/react-transitions/ReactTransitions.js
@@ -8,16 +8,15 @@ import TrainsitionSearch from './TransitionSearch';
 import { users } from 'common/utils/fakeUser';
 
 function ReactTransitions(props) {
-  
   const [transitionOn, setTransitionOn] = useState(false);
   const onValueChange = (event) => {
     const val = event.target.value;
     console.log(val);
-    if(val === "transition") {
-      setTransitionOn(true)}
-    else{
-      setTransitionOn(false)
-    };
+    if (val === 'transition') {
+      setTransitionOn(true);
+    } else {
+      setTransitionOn(false);
+    }
   };
 
   return (
@@ -25,27 +24,27 @@ function ReactTransitions(props) {
       <div className="play-details">
         <PlayHeader play={props} />
         <div className="react-transition play-details-body">
-        <div className="modes">
-          <input 
-            id="priority-id"
-            type="radio" 
-            value="priority" 
-            name="type"
-            checked={transitionOn === false}
-            onChange={onValueChange} /> 
-          <label htmlFor="priority-id">Priority</label>
-          <input 
-            id="transition-id"
-            type="radio" 
-            value="transition" 
-            name="type"
-            checked={transitionOn === true}
-            onChange={onValueChange} />
-          <label htmlFor="priority-id">Transition</label>
-        </div>
-          {transitionOn ? 
-              <TrainsitionSearch users={users} /> :
-              <PrioritySearch users={users} />}
+          <div className="modes">
+            <input
+              id="priority-id"
+              type="radio"
+              value="priority"
+              name="type"
+              checked={transitionOn === false}
+              onChange={onValueChange}
+            />
+            <label htmlFor="priority-id">Priority</label>
+            <input
+              id="transition-id"
+              type="radio"
+              value="transition"
+              name="type"
+              checked={transitionOn === true}
+              onChange={onValueChange}
+            />
+            <label htmlFor="priority-id">Transition</label>
+          </div>
+          {transitionOn ? <TrainsitionSearch users={users} /> : <PrioritySearch users={users} />}
         </div>
       </div>
     </>
