c919251b6e2a526d4a978ccaa1d3d6e598f65a11
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-07-05 18:01:56 +0530

    Added radio button switch and cover image

--- src/plays/react-transitions/ReactTransitions.js
+++ src/plays/react-transitions/ReactTransitions.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PlayHeader from 'common/playlists/PlayHeader';
 import './styles.css';
 
@@ -6,17 +7,45 @@ import TrainsitionSearch from './TransitionSearch';
 
 import { users } from 'common/utils/fakeUser';
 
-// WARNING: Do not change the entry componenet name
 function ReactTransitions(props) {
-  // Your Code Start below.
+  
+  const [transitionOn, setTransitionOn] = useState(false);
+  const onValueChange = (event) => {
+    const val = event.target.value;
+    console.log(val);
+    if(val === "transition") {
+      setTransitionOn(true)}
+    else{
+      setTransitionOn(false)
+    };
+  };
 
   return (
     <>
       <div className="play-details">
         <PlayHeader play={props} />
         <div className="react-transition play-details-body">
-          {/*<PrioritySearch users={users} />*/}
-          <TrainsitionSearch users={users} />
+        <div className="modes">
+          <input 
+            id="priority-id"
+            type="radio" 
+            value="priority" 
+            name="type"
+            checked={transitionOn === false}
+            onChange={onValueChange} /> 
+          <label htmlFor="priority-id">Priority</label>
+          <input 
+            id="transition-id"
+            type="radio" 
+            value="transition" 
+            name="type"
+            checked={transitionOn === true}
+            onChange={onValueChange} />
+          <label htmlFor="priority-id">Transition</label>
+        </div>
+          {transitionOn ? 
+              <TrainsitionSearch users={users} /> :
+              <PrioritySearch users={users} />}
         </div>
       </div>
     </>
