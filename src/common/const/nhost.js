8e78fde9dda25063f7be89d33248ee62c8836237
Angra974 <thierryparlier97440@gmail.com>
2023-07-18 13:04:49 +0400

    formatted output

--- src/common/const/nhost.js
+++ src/common/const/nhost.js
@@ -1,7 +1,6 @@
 const AUTH_URL = (redirectURL, provider = 'github') => {
   return `${
-    process.env.REACT_APP_NHOST_BACKEND_URL
-  }/${process.env.REACT_APP_NHOST_VERSION}/auth/signin/provider/${provider}?redirectTo=${encodeURI(redirectURL)}`;
+    process.env.REACT_APP_NHOST_BACKEND_URL}/${process.env.REACT_APP_NHOST_VERSION}/auth/signin/provider/${provider}?redirectTo=${encodeURI(redirectURL)}`;
 };
 
 export const NHOST = { AUTH_URL };
