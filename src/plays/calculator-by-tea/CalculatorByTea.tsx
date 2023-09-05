03edab721be640906ec6229cbde077820c4af3f8
Chaitanya Agarwal <chaipanch98@gmail.com>
2023-09-05 18:31:17 +0530

    feat: play added for #1232
    fixes issue  #1232

---
+++ src/plays/calculator-by-tea/CalculatorByTea.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PlayHeader from 'common/playlists/PlayHeader';
+import './styles.css';
+import { calculatorValues } from './CalculatorByTeaButtons';
+import useCalcalulatorByTeaHook from './useCalcalulatorByTeaHook';
+import { expression } from 'mathjs';
+
+function CalculatorByTea(props: any) {
+  const {
+    calculationExpression,
+    calculationHistory,
+    onInputRecived,
+    buttonClasses,
+    handleButtonClick
+  } = useCalcalulatorByTeaHook();
+
+  return (
+    <>
+      <div className="play-details">
+        <PlayHeader play={props} />
+        <div className="play-details-body">
+          {/* Your Code Starts Here */}
+          <div className="calculator">
+            <div className="calculator-body">
+              <div className="calculator-display-block">
+                {calculationHistory.map((expression) => {
+                  return <p className="calculator-history-text">{expression}</p>;
+                })}
+                <input
+                  className="calculator-input-box"
+                  type="text"
+                  value={calculationExpression}
+                  onChange={(e) => {
+                    onInputRecived(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="calculator-input-block">
+                <div className="calculator-value-block">
+                  {calculatorValues.map((value) => {
+                    return (
+                      <button
+                        className={`calculator-button ${buttonClasses(value.type)}`}
+                        key={value.data}
+                        onClick={() => {
+                          handleButtonClick(value);
+                        }}
+                      >
+                        {value.data}
+                      </button>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Your Code Ends Here */}
+      </div>
+    </>
+  );
+}
+
+export default CalculatorByTea;
---
+++ src/plays/calculator-by-tea/Readme.md
@@ -0,0 +1,27 @@
+# Calculator By Tea
+
+A calculator that performs basics operations
+
+## Play Demographic
+
+- Language: ts
+- Level: Beginner
+
+## Creator Information
+
+- User: MrTea7171
+- Gihub Link: https://github.com/MrTea7171
+- Blog:
+- Video:
+
+## Implementation Details
+
+Update your implementation idea and details here
+
+## Consideration
+
+Update all considerations(if any)
+
+## Resources
+
+Update external resources(if any)
