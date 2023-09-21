b5a685a0a307e5b7243f82c818dd83b6cb303210
Angra974 <thierryparlier97440@gmail.com>
2023-09-21 20:43:54 +0400

    Update cypress run timeout

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
       // implement node event listeners here
     },
     baseUrl: 'http://localhost:3000',
-    watchForFileChanges: false
+    watchForFileChanges: false,
+	pageLoadTimeout=100000
   }
 });
