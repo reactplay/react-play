8ca4c1aff9c76d8563c4046e98a1929ebb575773
Angra974 <thierryparlier97440@gmail.com>
2023-09-22 20:25:08 +0400

    linted before branch indexJson

---
+++ cypress/cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000',
+    watchForFileChanges: false,
+	pageLoadTimeout=100000,
+	responseTimeout=100000,
+	
+  }
+});
