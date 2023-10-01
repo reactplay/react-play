451040214d910cf3b67911a163e607e43f867f5a
ujwal-yadav <ujwalofficial99@gmail.com>
2023-10-01 12:54:41 +0530

    feat(common): add categorization on tech stack page

--- src/common/techstack/TechStackInfo.js
+++ src/common/techstack/TechStackInfo.js
@@ -21,90 +21,105 @@ export const TechStackInfo = [
     comp: SiReact,
     text: 'ReactJS',
     type: 'icon',
-    link: 'https://reactjs.org'
+    link: 'https://reactjs.org',
+    categoryID: 'frontend'
   },
   {
     comp: SiRedux,
     text: 'Redux',
     type: 'icon',
-    link: 'https://redux.js.org'
+    link: 'https://redux.js.org',
+    categoryID: 'frontend'
   },
   {
     comp: NHost,
     text: 'NHost',
     type: 'image',
-    link: 'https://nhost.io'
+    link: 'https://nhost.io',
+    categoryID: 'backend'
   },
   {
     comp: SiGit,
     text: 'Git',
     type: 'icon',
-    link: 'https://git-scm.com'
+    link: 'https://git-scm.com',
+    categoryID: 'devOpsAndDeployment'
   },
   {
     comp: SiTailwindcss,
     text: 'Tailwind',
     type: 'icon',
-    link: 'https://tailwindcss.com'
+    link: 'https://tailwindcss.com',
+    categoryID: 'frontend'
   },
   {
     comp: SiHasura,
     text: 'Hasura',
     type: 'icon',
-    link: 'https://hasura.io'
+    link: 'https://hasura.io',
+    categoryID: 'backend'
   },
   {
     comp: SiPostgresql,
     text: 'PostgreSQL',
     type: 'icon',
-    link: 'https://www.postgresql.org'
+    link: 'https://www.postgresql.org',
+    categoryID: 'backend'
   },
   {
     comp: SiJavascript,
     text: 'Javascript',
     type: 'icon',
-    link: 'https://www.ecma-international.org/'
+    link: 'https://www.ecma-international.org/',
+    categoryID: 'programmingLanguage'
   },
   {
     comp: SiFirebase,
     text: 'Firebase',
     type: 'icon',
-    link: 'https://firebase.google.com'
+    link: 'https://firebase.google.com',
+    categoryID: 'backend'
   },
   {
     comp: SiTypescript,
     text: 'Typescript',
     type: 'icon',
-    link: 'https://www.typescriptlang.org'
+    link: 'https://www.typescriptlang.org',
+    categoryID: 'programmingLanguage'
   },
   {
     comp: SiGraphql,
     text: 'Graphql',
     type: 'icon',
-    link: 'https://graphql.org'
+    link: 'https://graphql.org',
+    categoryID: 'backend'
   },
   {
     comp: SiDocker,
     text: 'Docker',
     type: 'icon',
-    link: 'https://www.docker.com'
+    link: 'https://www.docker.com',
+    categoryID: 'devOpsAndDeployment'
   },
   {
     comp: SiNodedotjs,
     text: 'NodeJS',
     type: 'icon',
-    link: 'https://nodejs.org/en'
+    link: 'https://nodejs.org/en',
+    categoryID: 'backend'
   },
   {
     comp: SiPython,
     text: 'Python',
     type: 'icon',
-    link: 'https://www.python.org'
+    link: 'https://www.python.org',
+    categoryID: 'programmingLanguage'
   },
   {
     comp: Umami,
     text: 'Umami',
     type: 'image',
-    link: 'https://umami.is/'
+    link: 'https://umami.is/',
+    categoryID: 'analyticsAndMonitoring'
   }
 ];
