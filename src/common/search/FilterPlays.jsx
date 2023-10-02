cb6b439d241e4744783fce8986524ef7848822f8
Sudip Kundu <sudipkundu999@gmail.com>
2023-10-02 13:42:06 +0530

    fix #1271

--- src/common/search/FilterPlays.jsx
+++ src/common/search/FilterPlays.jsx
@@ -108,17 +108,8 @@ const FilterPlays = ({ onChange, query }) => {
         finalQueryObject[key] = res[key];
       }
     });
-    const final_query = { ...finalQueryObject };
-    const final_query_param = new URLSearchParams(finalQueryObject).toString();
-
-    if (final_query_param) {
-      setShowModal(false);
-    } else {
-      setShowModal(true);
-    }
-    if (onChange) {
-      onChange(final_query);
-    }
+    setShowModal(false);
+    onChange({ ...finalQueryObject });
   };
 
   const handleChange = (key, value) => {
