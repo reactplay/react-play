2b1c4abb0b2d76c59a2c51475fb9df2e47fa642a
faisalmit@2502 <faisalirfan2502@gmail.com>
2023-10-02 17:18:48 +0530

    adding FaisalPlay first time

---
+++ src/plays/weatherapp/Readme.md
@@ -0,0 +1,27 @@
+# FaisalPlay
+
+great play lovely play enjoy play
+
+## Play Demographic
+
+- Language: ts
+- Level: Beginner
+
+## Creator Information
+
+- User: irfanmit
+- Gihub Link: https://github.com/irfanmit
+- Blog: https://therandomblogspot.com/
+- Video: https://www.youtube.com/watch?v=l240LuiMENA
+
+## Implementation Details
+
+Update your implementation idea and details here
+
+## Consideration
+
+Update all considerations(if any)
+
+## Resources
+
+Update external resources(if any)
---
+++ src/plays/weatherapp/WeatherApp.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import PlayHeader from 'common/playlists/PlayHeader';
+import './styles.css';
+
+function Faisalplay(props) {
+  const [currentWeatherData, setCurrentWeatherData] = useState(null);
+  const [cityName, setCityName] = useState('');
+  const apiKey = 'c60fc2840593482a76adfdac672e8ee9';
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setError(null);
+    setLoading(true);
+
+    // Make an asynchronous GET request to the API
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return response.json(); // Parse the response body as JSON
+      })
+      .then((data) => {
+        setCurrentWeatherData(data); // Set the weather data in the state
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError('Network error or invalid search');
+        setLoading(false);
+        console.error('Error:', error); // Log any errors to the console
+      });
+  };
+
+  return (
+    <>
+      <div className="play-details">
+        <PlayHeader play={props} />
+        <div className="play-details-body">
+          <div>
+            <input
+              placeholder="Search any city name"
+              value={cityName}
+              onChange={(e) => setCityName(e.target.value)} // Update the cityName state as the user types
+            />
+            <button onClick={handleClick}>Search</button>
+
+            {loading && <div className="spinner" />}
+
+            {error && <h2 className="error-message">{error}</h2>}
+
+            {currentWeatherData && !loading && !error && (
+              <div className="data_div">
+                <h2>
+                  Weather in {currentWeatherData.name}, {currentWeatherData.sys.country}
+                </h2>
+                <p>Temperature: {(currentWeatherData.main.temp - 273.15).toFixed(2)} C</p>
+                <p>Humidity: {currentWeatherData.main.humidity}%</p>
+                <p>Condition: {currentWeatherData.weather[0].main}</p>
+                <p>Description: {currentWeatherData.weather[0].description}</p>
+                <img
+                  alt={currentWeatherData.weather[0].main}
+                  src={`https://openweathermap.org/img/w/${currentWeatherData.weather[0].icon}.png`}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Faisalplay;
