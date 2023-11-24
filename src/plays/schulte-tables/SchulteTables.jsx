dcb825e2aef0d70a43af460cf6579c46967c4c6c
Mehdi Hasan Shohan <69374096+mhShohan@users.noreply.github.com>
2023-11-24 11:58:50 +0600

    Remove comment of SchulteTables

--- src/plays/schulte-tables/SchulteTables.jsx
+++ src/plays/schulte-tables/SchulteTables.jsx
@@ -2,18 +2,13 @@ import './styles.css';
 import PlayHeader from 'common/playlists/PlayHeader';
 import GameTable from './GameTable';
 
-// WARNING: Do not change the entry componenet name
 function SchulteTables(props) {
-  // Your Code Start below.
-
   return (
     <>
       <div className="play-details">
         <PlayHeader play={props} />
         <div className="play-details-body">
-          {/* Your Code Starts Here */}
           <GameTable />
-          {/* Your Code Ends Here */}
         </div>
       </div>
     </>
