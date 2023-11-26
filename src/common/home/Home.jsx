1a92c1c5e2daed5205a6ce3a7402aed862dc3521
notvanshh <vansh@skords.com>
2023-11-27 01:18:36 +0530

    Re-render bug fix: Removed hooks as not being used anywhere.

--- src/common/home/Home.jsx
+++ src/common/home/Home.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useFetch from 'common/hooks/useFetch';
+import { useSearchContext } from 'common/search/search-context';
 import './home.css';
 import HomeBanner from './HomeBanner';
 import HomeContent from './HomeContent';
