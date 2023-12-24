6a5989a95713b337408c97b60299743dbf63f973
Harshit Jain <hjain.tech@gmail.com>
2023-12-24 23:34:54 +0530

    fix(common): focus fix on play filter modal open

--- src/common/modal/index.jsx
+++ src/common/modal/index.jsx
@@ -1,8 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { GoCheck, GoX } from 'react-icons/go';
 
 const Modal = ({ title, show, onClose, onSubmit, children, cname }) => {
+  const modalChildrenRef = useRef(null);
+
+  // useEffect to set the focus on the first focussable element in the modal
+  // when it opens up
+  useEffect(() => {
+    if (show) {
+      const modalChildElements = modalChildrenRef.current;
+      const focusableElements = modalChildElements.querySelectorAll(
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+      );
+      const firstFocusableElement = focusableElements[0];
+      firstFocusableElement.focus();
+    }
+  }, [show]);
+
   useEffect(() => {
     const close = (e) => {
       // e.keyCode is deprecated: developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode.
@@ -25,7 +40,9 @@ const Modal = ({ title, show, onClose, onSubmit, children, cname }) => {
         <div className={`modal-${cname}-header`}>
           <h2 className="modal-title">{title}</h2>
         </div>
-        <div className={`modal-${cname}-body`}>{children}</div>
+        <div className={`modal-${cname}-body`} ref={modalChildrenRef}>
+          {children}
+        </div>
         <div className={`modal-${cname}-footer`}>
           <button className="btn-default-light btn-size--sm" onClick={onClose}>
             <GoX className="icon" size="16px" /> Cancel
