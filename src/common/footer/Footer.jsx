899e7dd9ca449704c919b3f2bd41aee8973a67e9
prasen aarthi <prasen.aarthi@gmail.com>
2024-02-14 16:35:37 +0530

    #1450- BUG FIX : footer position

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="app-footer text-center">
+    <footer className="app-footer text-center absolute left-0 right-0 bottom-0 w-full">
       <hr className="separater" />
       <div className="flex justify-center items-center py-2">
         <p className="app-footer-text">
