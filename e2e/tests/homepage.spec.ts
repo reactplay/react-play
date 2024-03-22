a05d01cdf45c3ac109be9d133acc43b2066b5aec
Mohat Shil <146836180+mohatShilABB@users.noreply.github.com>
2024-03-22 01:21:10 +0100

    added more devices to test & completed homepage
    - firefox and chrome added
    - completed homepage fully (need to convert to po still)
    - create npm/yarn e2e script

--- e2e/tests/homepage.spec.ts
+++ e2e/tests/homepage.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
+  await page.waitForLoadState('domcontentloaded');
 });
 
 test('Header section is visible and btns are editable', async ({ page }) => {
@@ -60,8 +61,86 @@ test('Ideas section is visible & goes to proper /pages ', async ({ page }) => {
   await page.goBack();
 });
 
-test.only('Trending Plays section is visible, has four view, is clickable & goes to proper /pages ', async ({
+test('Trending Plays section is visible, has four view & goes to proper /pages ', async ({
   page
 }) => {
-  await page.pause();
+  const plays = page.locator('[class="home-plays"]');
+  await plays.scrollIntoViewIfNeeded();
+  await expect(plays.locator('[class="plays-title-primary"]')).toContainText('Trending Plays');
+  await page.waitForLoadState('networkidle');
+  // counts cards based on four links & added "View all play string" makes it five
+  const cardCount = plays.getByRole('link').count();
+  expect(await cardCount).toEqual(5);
+  // nav check
+  await plays.getByRole('link', { name: 'View all Plays' }).click();
+  expect(page.url()).toMatch(/plays/);
+  await page.goBack();
+});
+
+test('What our section - testimonials is visible, slider is clickable & str to proper /pages ', async ({
+  page
+}) => {
+  const testimonials = page.locator('[class="testimonials"]');
+  await testimonials.scrollIntoViewIfNeeded();
+  await page.waitForLoadState('networkidle');
+  await expect(testimonials.locator('[class="title-primary"]')).toContainText(
+    'What Our Community Says!'
+  );
+  // pagination swipier check
+  await expect(testimonials.locator('.swiper-pagination')).toBeEditable();
+  await expect(testimonials.locator('[class*="swiper swiper-initialized"]')).toBeVisible();
+  // nav check
+  await testimonials.getByRole('link', { name: 'View all Testimonials' }).click();
+  expect(page.url()).toMatch(/testimonials/);
+  await page.goBack();
+});
+
+test('big thanks to Contributors is visible ', async ({ page }) => {
+  const contributors = page.locator('[data-testid="contributors-section"]');
+  await contributors.scrollIntoViewIfNeeded();
+  await page.waitForLoadState('networkidle');
+  expect(contributors).toBeInViewport;
+  await expect(contributors.locator('[class="title-primary"]')).toContainText(
+    'Big Thanks to All Contributors!'
+  );
+});
+test('Our Partners & Sponsors is visible, links are clickable and has links ', async ({ page }) => {
+  const partnersSponsers = page.locator('[data-testid="sponsors-section"]');
+  await partnersSponsers.scrollIntoViewIfNeeded();
+  await page.waitForLoadState('networkidle');
+  expect(partnersSponsers).toBeInViewport;
+  await expect(partnersSponsers.locator('[class="title-primary"]')).toContainText(
+    'OurPartners & Sponsors'
+  );
+  // total 10 links presents check
+  expect(await partnersSponsers.getByRole('link').count()).toEqual(10);
+
+  // backers
+  await expect(partnersSponsers.locator('[class="backers-container"]')).toContainText('Backers');
+  await expect(partnersSponsers.getByText('Support us with any monthly')).toContainText(
+    'Support us with any monthly sponsorship. Your help means a lot to us. We will give you a shoutout and add you here.'
+  );
+  await expect(partnersSponsers.locator('[class="prime-sponsors-container"]')).toContainText(
+    'Prime Sponsors'
+  );
+  await expect(partnersSponsers.getByText('Be a prime sponsor of')).toContainText(
+    'Be a prime sponsor of ReactPlay. Your sponsorship will go a long way, and we will place you in our internal events, newsletters and here on the homepage.'
+  );
+});
+
+test('footer is visible, links are clickable and has links ', async ({ page }) => {
+  const partnersSponsers = page.locator('[class="app-footer--home app-footer"]');
+  await partnersSponsers.scrollIntoViewIfNeeded();
+  await page.waitForLoadState('networkidle');
+  expect(partnersSponsers).toBeInViewport;
+  await expect(partnersSponsers.locator('[class="body-primary"]')).toContainText(
+    'An open-source project made with ❤️ by team ReactPlay.'
+  );
+  await expect(partnersSponsers.getByRole('heading', { name: 'About' })).toBeVisible();
+  await expect(partnersSponsers.getByRole('heading', { name: 'Show love' })).toBeVisible();
+  // total 14 links
+  expect(await partnersSponsers.getByRole('link').count()).toEqual(14);
+  await partnersSponsers.getByRole('button', { name: 'Share about ReactPlay' }).click();
+  await expect(page.locator('[class*="modal-share"]').nth(0)).toBeVisible();
+  await page.getByRole('button', { name: 'Cancel' }).click();
 });
