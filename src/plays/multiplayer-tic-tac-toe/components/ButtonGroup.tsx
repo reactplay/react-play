aa1c6ac6715d6b607de13ca89580aae7276dc862
Rana Zaeem <149497053+RanaZaeem0@users.noreply.github.com>
2024-06-22 08:34:18 -0700

    Update ButtonGroup.tsx
    Signed-off-by: Rana Zaeem  <149497053+RanaZaeem0@users.noreply.github.com>

--- src/plays/multiplayer-tic-tac-toe/components/ButtonGroup.tsx
+++ src/plays/multiplayer-tic-tac-toe/components/ButtonGroup.tsx
@@ -11,14 +11,14 @@ export default function ButtonGroup({ setIsCross }: ButtonGroupProps) {
     <>
       <section className="flex justify-center">
         <button
-          className="flex justify-center items-center w-28 h-16 m-3 cursor-pointer border rounded-lg bg-[#00f2fe] hover:border-black"
+          className="grid justify-center items-center w-24 h-16 m-3 cursor-pointer border rounded-lg bg-[#00f2fe] hover:border-black"
           onClick={() => setIsCross(true)}
         >
           <span className="hidden">Cross</span>
           <FaTimes className="text-2xl font-extrabold text-red-500" />
         </button>
         <button
-          className="flex justify-center items-center w-28 h-16 m-3 cursor-pointer border rounded-lg bg-[#00f2fe] hover:border-black"
+          className="grid justify-center items-center w-24 h-16 m-3 cursor-pointer border rounded-lg bg-[#00f2fe] hover:border-black"
           onClick={() => setIsCross(false)}
         >
           <span className="hidden">Circle</span>
