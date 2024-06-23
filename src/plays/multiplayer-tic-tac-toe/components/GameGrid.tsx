372040347afd927da32d274049b9c6d72f5e4bb9
Rana Zaeem <149497053+RanaZaeem0@users.noreply.github.com>
2024-06-23 04:51:06 -0700

    Screen Css issue at both mobile and desktop view #1494 (#1503)
    * Update ButtonGroup.tsx
    
    Signed-off-by: Rana Zaeem  <149497053+RanaZaeem0@users.noreply.github.com>
    
    * Update GameGrid.tsx
    
    Signed-off-by: Rana Zaeem  <149497053+RanaZaeem0@users.noreply.github.com>
    
    * Update MultiplayerTicTacToe.tsx
    
    Signed-off-by: Rana Zaeem  <149497053+RanaZaeem0@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Rana Zaeem  <149497053+RanaZaeem0@users.noreply.github.com>

--- src/plays/multiplayer-tic-tac-toe/components/GameGrid.tsx
+++ src/plays/multiplayer-tic-tac-toe/components/GameGrid.tsx
@@ -12,7 +12,7 @@ export default function GameGrid({ changeItem }: GameGrid) {
       <div className="grid grid-cols-3 gap-4">
         {GAME_ARRAY.map((ele, i) => (
           <div
-            className="md:w-24 md:h-24 w-32 flex justify-center items-center h-32 bg-[#00f2f2] border-2 border-black rounded-lg cursor-pointer"
+            className="md:h-16  sm:w-44 w-20  gird justify-center items-center h-12  bg-[#00f2f2] border-2 border-black rounded-lg cursor-pointer"
             key={i}
             onClick={() => changeItem(i)}
           >
