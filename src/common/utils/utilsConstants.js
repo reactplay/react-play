56424d2a67b1a85ac5699de4d857d778b94bf15a
Md. Saddam Hossain <44530098+saddamhr@users.noreply.github.com>
2024-10-16 21:45:09 +0600

    (refactor) coverImageUtil, fakeUser and (#1553)
    formatCount methods inside src/common/utils/
    
    Co-authored-by: Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>

---
+++ src/common/utils/utilsConstants.js
@@ -0,0 +1,16 @@
+// cover image constants
+export const IMAGE_EXTENSIONS = ['png', 'jpg', 'jpeg'];
+export const FULFILLED_STATUS = 'fulfilled';
+
+// fake user constant
+export const USER_COUNT = 10000;
+
+// format format duration count constants
+export const SECONDS_IN_MINUTE = 60; // 60 seconds in a minute
+export const SECONDS_IN_HOUR = 3600; // 3600 seconds in an hour
+export const HOURS_THRESHOLD = 0; // Used for checking if hours are greater than 0
+
+// format view count constants
+export const BILLION = 1_000_000_000; // 1 billion
+export const MILLION = 1_000_000; // 1 million
+export const THOUSAND = 1_000; // 1 thousand
