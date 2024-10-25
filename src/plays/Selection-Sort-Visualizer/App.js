a55a11901f9f36856df2c4c6d549e700c9637341
ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
2024-10-25 10:10:12 +0530

    Added Selection Sort Visualizer Project in plays folder (#1563)
    * Create README.md
    
    Signed-off-by: ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
    
    * Create SelectionSortVisualizer.js
    
    Signed-off-by: ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
    
    * Create App.js
    
    Signed-off-by: ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
    
    * Create select.css
    
    Signed-off-by: ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
    
    * Update select.css
    
    Signed-off-by: ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
    
    * Update SelectionSortVisualizer.js
    
    Signed-off-by: ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
    Co-authored-by: Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>

---
+++ src/plays/Selection-Sort-Visualizer/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SelectionSortVisualizer from './SelectionSortVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <SelectionSortVisualizer />
+    </div>
+  );
+}
+
+export default App;
