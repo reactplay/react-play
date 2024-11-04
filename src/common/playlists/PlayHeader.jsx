3a48cc21bb4b49ce1bc3765ddae399c1ef4821ff
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-11-04 10:32:34 +0530

    Improved PlayHeader(#1562) (#1568)
    * Fixed Play Header
    
    Added collapse button and updated the max-width for the description.
    
    * made collapse mobile responsive
    
    * enhanced collapse button

--- src/common/playlists/PlayHeader.jsx
+++ src/common/playlists/PlayHeader.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlayHeaderInfo from './PlayHeaderInfo';
 import PlayHeaderActions from './PlayHeaderActions';
+import { FaAngleUp } from 'react-icons/fa6';
+import { FaAngleDown } from 'react-icons/fa6';
+
+export const CollapseContext = React.createContext();
 
 const PlayHeader = ({ play }) => {
+  const [collapse, setCollapse] = useState(false);
+
   return (
     <>
       <div className="play-details-header">
-        <PlayHeaderInfo play={play} />
+        <CollapseContext.Provider value={collapse}>
+          <PlayHeaderInfo play={play} />
+        </CollapseContext.Provider>
         <div className="header-rightcol">
           <div className="header-actions">
+            <button
+              className="collapse-btn px-3 py-2 rounded-md flex flex-row justify-center items-center text-white"
+              onClick={() => {
+                setCollapse((prev) => !prev);
+              }}
+            >
+              {collapse ? <FaAngleUp className="mr-2" /> : <FaAngleDown className="mr-2" />}
+              Collapse
+            </button>
             <PlayHeaderActions play={play} />
           </div>
-          <small className="header-desc">{play.description}</small>
+          {collapse && <small className="header-desc">{play.description}</small>}
         </div>
       </div>
     </>
