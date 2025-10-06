c46b985843736de30b266aa338c949641cc8dcc1
Sabeerwaqas <sabeerwaqas050@gmail.com>
2025-10-06 21:17:16 +0500

    fix: Zoomlogin component replaced code

--- src/plays/zoomlogin/Zoomlogin.js
+++ src/plays/zoomlogin/Zoomlogin.js
@@ -1,20 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './components/App';
-import HomePage from './components/HomePage';
-import LinkedinPage from './components/Linkedin/LinkedinPage';
-import ZoomPage from './components/Zoom/ZoomPage';
+import PlayHeader from 'common/playlists/PlayHeader';
+import './styles.css';
 
-export function MainPage() {
+import App from 'plays/zoomlogin/components/App';
+
+// WARNING: Do not change the entry componenet name
+function Zoomlogin(props) {
+  // Your Code Start below.
+
   return (
-    <BrowserRouter basename="/react-login-form">
-      <App>
-        <Routes>
-          <Route element={<HomePage />} path="/" />
-          <Route element={<LinkedinPage />} path="/linkedin" />
-          <Route element={<ZoomPage />} path="/zoom" />
-        </Routes>
-      </App>
-    </BrowserRouter>
+    <>
+      <div className="play-details">
+        <PlayHeader play={props} />
+        <div className="play-details-body">
+          {/* Your Code Starts Here */}
+          <App />
+          {/* Your Code Ends Here */}
+        </div>
+      </div>
+    </>
   );
 }
-export default MainPage;
+
+export default Zoomlogin;
