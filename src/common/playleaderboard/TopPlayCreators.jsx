d95438db5dd311f5c4b30f8ab42f0e82b2158289
Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>
2025-10-19 10:05:42 +0530

    Revert "feat: update leaderboard UI with top 3 highlights and trophy icons (#…" (#1622)
    This reverts commit 3048149afc02df6300223e5a228f13d9b168727d.

--- src/common/playleaderboard/TopPlayCreators.jsx
+++ src/common/playleaderboard/TopPlayCreators.jsx
@@ -4,38 +4,22 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import './leaderBoard.css';
 
 const TopPlayCreators = ({ topPlayCreators }) => {
-  const getRankIcon = (rank) => {
-    if (rank === 1) {
-      return <EmojiEventsIcon className="gold-trophy" fontSize="small" />;
-    }
-
-    if (rank === 2) {
-      return <EmojiEventsIcon className="silver-trophy" fontSize="small" />;
-    }
-
-    if (rank === 3) {
-      return <EmojiEventsIcon className="bronze-trophy" fontSize="small" />;
-    }
-
-    return null;
+  const profilePicture = (name, avatarUrl) => {
+    return (
+      <div className="flex flex-row items-center gap-4">
+        <img
+          alt={name}
+          className="rounded-full border-solid h-8 w-8"
+          src={avatarUrl}
+          title={name}
+        />
+        <div className="leaderboard-table-cell">{name}</div>
+      </div>
+    );
   };
-
-  const profilePicture = (name, avatarUrl, rank) => (
-    <div className="flex flex-row items-center gap-3">
-      {getRankIcon(rank)}
-      <img
-        alt={name}
-        className="h-8 w-8 rounded-full border-solid rank-avatar"
-        src={avatarUrl}
-        title={name}
-      />
-      <div className="leaderboard-table-cell">{name}</div>
-    </div>
-  );
 
   return (
     <TableContainer className="leaderboard-container">
@@ -46,34 +30,24 @@ const TopPlayCreators = ({ topPlayCreators }) => {
               Name
             </TableCell>
             <TableCell align="center" className="leaderboard-table-header">
-              Number of Plays
+              Number of plays
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {topPlayCreators.map((creator, index) => {
-            const rank = index + 1;
-
-            return (
-              <TableRow
-                className={`rank-${rank}`}
-                key={creator.displayName}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell
-                  align="left"
-                  className="leaderboard-table-cell"
-                  component="th"
-                  scope="row"
-                >
-                  {profilePicture(creator.displayName, creator.avatarUrl, rank)}
-                </TableCell>
-                <TableCell align="center" className="leaderboard-table-cell">
-                  {creator.count}
-                </TableCell>
-              </TableRow>
-            );
-          })}
+          {topPlayCreators.map((creator) => (
+            <TableRow
+              key={creator.displayName}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="left" className="leaderboard-table-cell" component="th" scope="row">
+                {profilePicture(creator.displayName, creator.avatarUrl)}
+              </TableCell>
+              <TableCell align="center" className="leaderboard-table-cell">
+                {creator.count}
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
