690482cd5b64ce0c950c57a662405df962e81a9c
Dhirajgithub813 <dasdhiraj813@gmail.com>
2026-02-17 21:32:42 +0530

    Add home and Back to top button

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -1,18 +1,24 @@
 import { Link } from 'react-router-dom';
+import BackToTop from 'common/components/BackToTop';
 
 const Footer = () => {
   return (
     <footer className="app-footer text-center">
       <hr className="separater" />
-      <div className="flex justify-center items-center py-2">
-        <p className="app-footer-text">
-          ReactPlay - The MIT License (MIT) Copyright &copy;
-          {new Date().getFullYear()} . Powered by{' '}
-          <Link className="text-link-default" to="/tech-stacks">
-            Top Notch Tech Stacks
-          </Link>
-          .
-        </p>
+      <div className="footer-content-wrapper">
+        <div className="flex justify-center items-center py-2">
+          <p className="app-footer-text">
+            ReactPlay - The MIT License (MIT) Copyright &copy;
+            {new Date().getFullYear()} . Powered by{' '}
+            <Link className="text-link-default" to="/tech-stacks">
+              Top Notch Tech Stacks
+            </Link>
+            .
+          </p>
+          <div className="footer-back-to-top">
+            <BackToTop isInFooter />
+          </div>
+        </div>
       </div>
     </footer>
   );
